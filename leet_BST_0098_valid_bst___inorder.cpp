3f4bf235bfa76f4d9ee37870804eaf5bb57d123b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-11 10:37:49 +0200

    xcviii . inorder traversal to array then check the array

--- leet_BST_0098_valid_bst___inorder.cpp
+++ leet_BST_0098_valid_bst___inorder.cpp
@@ -1,3 +1,51 @@
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root)
+    {
+        vector<int> group;
+        inorder(root, group);
+        print_vi(group);
+        int i = 0;
+        while (++i < group.size())
+            if (group[i - 1] >= group[i])
+                return false;
+        return true;
+    }
+
+    void    inorder(TreeNode *node, vector<int>& n)
+    {
+        if (!node)
+            return ;
+        inorder(node->left, n);
+        n.push_back(node->val);
+        inorder(node->right, n);
+    }
+
+    void    print_vi(vector<int> nums)
+    {
+        for (int n : nums)
+            cout << n << endl;
+        cout << endl;
+    }
+};
+
+/*
+
+[1,2,3,4,5,null,null]
+
+*/
+
+/*
+
+Inorder Algorithm
+   1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+   2. Visit the root.
+   3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+
+*/
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,28 +57,3 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-class Solution {
-public:
-    bool isValidBST(TreeNode* root)
-    {
-        vector<int> arr;
-
-        get_vals(root, arr);
-        int i = 0;
-        while (++i < (int) arr.size())
-        {
-            if (arr[i - 1] >= arr[i])    return false ;
-        }
-        return true ;
-    }
-    
-    void    get_vals(TreeNode *node, vector<int>& arr)
-    {
-        if (!node)  return ;
-        get_vals(node->left, arr);
-        arr.push_back(node->val);
-        get_vals(node->right, arr);
-    }
-};
-
