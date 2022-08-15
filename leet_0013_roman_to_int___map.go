cb209a6e4134dc625618b5669f1377ac83703f43
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-15 23:59:28 +0200

    + xiii . update golang using map . rune to string

--- leet_0013_roman_to_int___map.go
+++ leet_0013_roman_to_int___map.go
@@ -1,25 +1,17 @@
 func romanToInt(s string) int {
     dp := map[string] int {
+
         // voldemort group
         "V":  5,
         "L":  50,
 	"D":  500,
         "M":  1000,
 
-	// i group
+	// i x c group
         "I":  1,
-	"IV": 4,
-        "IX": 9,
-
-        // x group
 	"X":  10,
-	"XL": 40,
-	"XC": 90,
-
-        // c Group
 	"C":  100,
-	"CD": 400,
-	"CM": 900,
+
     }
     size := len(s)
     res := 0
