626a8969e478b8fc2dbd5a183dd92de424df9ebe
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-09-01 00:00:12 +0200

    cclxxviii . add rust itera + recur

---
+++ leet_BS_278_first_bad.rs
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// isBadVersion(version:i32)-> bool;
+// to call it use self.isBadVersion(version)
+
+impl Solution {
+    pub fn first_bad_version(&self, n: i32) -> i32
+    {
+        let mut l: i32 = 1;
+        let mut r: i32 = n;
+
+        while (l <= r)
+        {
+            let     mid: i32 = l + (r - l) / 2;
+
+            if (self.isBadVersion(mid)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1
+            }
+        }
+        return l;
+    }
+}
---
+++ leet_BS_278_first_bad___recurse.rs
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// isBadVersion(version:i32)-> bool;
+// to call it use self.isBadVersion(version)
+
+impl Solution {
+    pub fn first_bad_version(& self, n: i32) -> i32 {
+		Solution::binary_search(&self, 0, n, n)
+    }
+
+    fn binary_search(&self, l: i32, r: i32, curr: i32) -> i32 {
+        if (l > r) {
+            return curr;
+        }
+        let mid: i32 = l + (r - l) / 2;
+        if (self.isBadVersion(mid)) 
+        {
+            return Solution::binary_search(&self, l, mid - 1, mid);
+        }
+        return Solution::binary_search(&self, mid + 1, r, curr);
+    }
+}
