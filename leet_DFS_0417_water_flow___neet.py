2a1da349bb6ad4baf8b79bc9b4e25b47e620a94a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-31 11:27:25 +0200

    cdxvii . graph problem

---
+++ leet_DFS_0417_water_flow___neet.py
@@ -0,0 +1,29 @@
+class Solution:
+    def pacificAtlantic(self, h: List[List[int]]) -> List[List[int]]:
+        def DFS(i, j, r, c, visited, lastpos) :
+            if i < 0 or j < 0 or i > r - 1 or j > c - 1 or \
+                (i, j) in visited or h[i][j] < lastpos:
+                    return
+            visited.add((i, j))
+            DFS(i + 1, j, r, c, visited, h[i][j])
+            DFS(i - 1, j, r, c, visited, h[i][j])
+            DFS(i, j + 1, r, c, visited, h[i][j])
+            DFS(i, j - 1, r, c, visited, h[i][j])
+        r = len(h)
+        c = len(h[0])
+        pac = set() # two visited sets
+        alt = set()
+        for i in range(r):
+            DFS(i, 0, r, c, pac, h[i][0]) # start from left col ie. [i][0]
+            DFS(i, c - 1, r, c, alt, h[i][c - 1])
+        for i in range(c):
+            DFS(0, i, r, c, pac, h[0][i])
+            DFS(r - 1, i, r, c, alt, h[r - 1][i])
+        # print(pac)
+        # print(alt)
+        res = []
+        for i in range(r):
+            for j in range(c):
+                if (i, j) in pac and (i, j) in alt:
+                    res.append([i, j])
+        return res
