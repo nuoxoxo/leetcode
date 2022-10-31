72a6a36fd67c724fe470aef646bfcac938de41f7
nuoxoxo <nuxu@e3r3p12.clusters.42paris.fr>
2022-10-31 12:54:03 +0100

    ud. dcclxxvi . cpp

--- leet_766_toeplitz_matrix.cpp
+++ leet_766_toeplitz_matrix.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix)
     {
-        vector<int> standard = matrix[0];
-        int         n = matrix[0].size();
-        int         m = matrix.size();
-        int         i = 0;
+        vector<int>	standard = matrix[0];
+        int		n = matrix[0].size();
+        int		m = matrix.size();
+        int		i = 0;
 
-        if (m == 1 || n == 1)   return true ;
+        if (m == 1 || n == 1)
+		return true ;
         while (++i < m)
         {
-            vector<int> current = matrix[i];
+            vector<int>		current = matrix[i];
+
             standard.pop_back();
             if (!std::equal(standard.begin(), standard.end(), current.begin() + 1))
                 return false ;
@@ -19,3 +21,4 @@ public:
         return true ;
     }
 };
+
