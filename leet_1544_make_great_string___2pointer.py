75cbadef10b246eefdcb0eaaf63467ed539b3644
nuoxoxo <nuxu@e3r2p16.clusters.42paris.fr>
2022-11-08 14:26:25 +0100

    + mdxliv . edit a bug

--- leet_1544_make_great_string___2pointer.py
+++ leet_1544_make_great_string___2pointer.py
@@ -4,10 +4,11 @@ class Solution:
         i = 0
         j = -1
         while i < len(s):
-            if j == -1 or abs(ord(s[j]) - ord(s[i])) != 32:
+            if j == -1 or abs(ord(a[i]) - ord(a[j])) != 32:
                 j += 1
                 a[j] = a[i]
             else:
                 j -= 1
             i += 1
-        return ''.join(a[:j + 1])
+        return ''.join(a[0:j + 1])
+
