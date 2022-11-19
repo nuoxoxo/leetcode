794c4c52f62be07f829b085e92436dd88b015109
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-19 01:20:05 +0100

    ed. py

--- leet_0561_array_partition.py
+++ leet_0561_array_partition.py
@@ -3,5 +3,5 @@ class Solution:
         res = 0
         nums.sort()
         for i in range(0, len(nums), 2):
-            res += min(nums[i], nums[i + 1])
+            res += nums[i]
         return res
