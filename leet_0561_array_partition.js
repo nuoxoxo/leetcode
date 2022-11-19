f57eb88510461977970008e9cf0e89ee33fe8361
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-19 01:20:00 +0100

    ed. Js

--- leet_0561_array_partition.js
+++ leet_0561_array_partition.js
@@ -2,12 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var arrayPairSum = function(nums) {
     nums.sort((a, b) => a - b)
     let i = 0
     let res = 0
     while (i < nums.length) {
-        res += Math.min(nums[i], nums[i + 1])
+        res += nums[i]
         i += 2
     }
     return res
