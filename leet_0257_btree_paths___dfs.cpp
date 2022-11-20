3e6fd8f44b041e5acfcbce186940a436267cc16d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-20 18:28:59 +0100

    added cc;vii . DFS in cpp

---
+++ leet_0257_btree_paths___dfs.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    vector<string>  binaryTreePaths(TreeNode* root)
+    {
+        vector<string>      res;
+
+        if (root)
+            DFS(res, root, "");
+        return (res);
+    }
+
+    void DFS(vector<string> & v, TreeNode *node, string s)
+    {
+        if ( !(node->left) && !(node->right))
+            v.push_back(s + std::to_string(node->val));
+        else
+        {
+            if (node->left)
+                DFS(v, node->left, s + std::to_string(node->val) + "->");
+            if (node->right)
+                DFS(v, node->right, s + std::to_string(node->val) + "->");
+        }
+    }
+};
