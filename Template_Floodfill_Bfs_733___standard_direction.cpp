8fc592f090eb42738fbf75872005efcf63e36237
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-21 11:06:03 +0100

    upd. cpp soln. using dir vector

--- Template_Floodfill_Bfs_733___standard_direction.cpp
+++ Template_Floodfill_Bfs_733___standard_direction.cpp
@@ -10,30 +10,23 @@ public:
         int     r = img.size();
         int     i, j;
 
-        deque<pair<int, int>>   dq;
+        vector<vector<int>>     dir = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
+        deque<pair<int, int>>   dq;
         dq.push_back({x, y});
+
         while (!empty(dq))
         {
             i = dq.front().first;
             j = dq.front().second;
             img[i][j] = nc;
             dq.pop_front();
-            if (i + 1 < r && img[i + 1][j] == oc)
+            for (vector<int> d: dir)
             {
-                dq.push_back( {i + 1, j} );
-            }
-            if (i - 1 > -1 && img[i - 1][j] == oc)
-            {
-                dq.push_back( {i - 1, j} );
-            }
-            if (j + 1 < c && img[i][j + 1] == oc)
-            {
-                dq.push_back( {i, j + 1} );
-            }
-            if (j - 1 > -1 && img[i][j - 1] == oc)
-            {
-                dq.push_back( {i, j - 1} );
+                x = i + d[0];
+                y = j + d[1];
+                if (x < r && x > -1 && y < c && y > -1 && img[x][y] == oc)
+                    dq.push_back({x, y});
             }
         }
         return img;
