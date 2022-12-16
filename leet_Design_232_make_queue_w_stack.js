34289562f04a9a1e7836196dc7fa12c013571258
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-16 06:57:38 +0100

    ccxxxi . revisited . added Js

---
+++ leet_Design_232_make_queue_w_stack.js
@@ -0,0 +1,48 @@
+
+var MyQueue = function() {
+    this.arr = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+
+MyQueue.prototype.push = function(x) {
+    this.arr.push(x)
+};
+
+/**
+ * @return {number}
+ */
+
+MyQueue.prototype.pop = function() {
+    if (this.arr.length < 1)
+        return
+    return this.arr.shift()
+};
+
+/**
+ * @return {number}
+ */
+
+MyQueue.prototype.peek = function() {
+    return this.arr[0]
+};
+
+/**
+ * @return {boolean}
+ */
+
+MyQueue.prototype.empty = function() {
+    return this.arr == 0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
