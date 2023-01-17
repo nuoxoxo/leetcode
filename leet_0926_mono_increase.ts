f3894e95111c6cfbfa6db680d1a8cc0049684c12
nuoxoxo <nuxu@e1r2p10.clusters.42paris.fr>
2023-01-17 15:16:37 +0100

    mcxxvi mono increasing . ts js

---
+++ leet_0926_mono_increase.ts
@@ -0,0 +1,13 @@
+var minFlipsMonoIncr = function(s) {
+    let z = 0
+    let o = 0
+    for (let c of s) {
+        if (c == '1') {
+            o++
+        } else {
+            z++
+        }
+        z = z < o ? z : o
+    }
+    return z
+};
