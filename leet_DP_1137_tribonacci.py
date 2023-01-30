c5b1052261c5cf133dcb8bebe5eca7907188f8bd
nuoxoxo <nuxu@e1r5p8.clusters.42paris.fr>
2023-01-30 10:15:09 +0100

    1137 update python

--- leet_DP_1137_tribonacci.py
+++ leet_DP_1137_tribonacci.py
@@ -1,14 +1,10 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n is 0:
+        if n < 2:
             return n
-        if n < 3:
-            return 1
-        g = [None] * (1 + n)
-        g[0] = 0
-        g[1] = g[2] = 1
-        i = 3
-        while i < n + 1:
-            g[i] = g[i - 3] + g[i - 2] + g[i - 1]
-            i += 1
-        return g[n]
+        f = [0 for _ in range(n + 1)]
+        f[1] = 1
+        f[2] = 1
+        for i in range(3, n + 1):
+            f[i] = f[i - 3] + f[i - 2] + f[i - 1]
+        return f[n] 
