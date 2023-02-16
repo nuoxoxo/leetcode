3d75d014076164a6edafdebbbc7562613095388f
nuoxoxo <nuxu@e1r3p7.clusters.42paris.fr>
2023-02-16 08:02:27 +0100

    upd. 104 . bfs in js

--- leet_BFS_0104_max_depth_btree.js
+++ leet_BFS_0104_max_depth_btree.js
@@ -1,17 +1,30 @@
-/// BFS
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var maxDepth = function(root) {
-    if (!root)  return (0)
-    let E = [ root ]
-    let depth = 0
-    while (E.length != 0) {
-        let i = -1, len = E.length
-        while (++i < len) {
-            let node = E.shift()
-            if (node.left)  E.push(node.left)
-            if (node.right) E.push(node.right)
+    if (root == null)
+        return 0
+    let arr = [root]
+    let res = 0;
+    while (arr.length !== 0) {
+        let len = arr.length
+        while (--len > -1) {
+            let node = arr.shift()
+            if (node.left)
+                arr.push(node.left)
+            if (node.right)
+                arr.push(node.right)
         }
-        ++ depth
+        ++res;
     }
-    return (depth)
+    return res;
 };
