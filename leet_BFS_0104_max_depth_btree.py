03ca21af83c545eabc627781bf9f0317436f0ad5
nuoxoxo <nuxu@e1r3p7.clusters.42paris.fr>
2023-02-16 08:02:09 +0100

    upd. 104 . bfs in py

--- leet_BFS_0104_max_depth_btree.py
+++ leet_BFS_0104_max_depth_btree.py
@@ -4,21 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
-"""BFS"""
-
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+        if not root:
             return 0
-        E = deque( [root] )
-        depth = 0
-        while E:
-            for _ in range(len(E)):
-                node = E.popleft()
-                if node.left:   E.append(node.left)
-                if node.right:  E.append(node.right)
-            depth += 1
-        return depth
+        D = deque([root])
+        res = 0
+        while D:
+            for i in range(len(D)):
+                node = D.popleft()
+                if node.left:
+                    D.append(node.left)
+                if node.right:
+                    D.append(node.right)
+            res += 1
+        return res
+
