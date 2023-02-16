7058e74f0624092c8e4acba3b61bd17880afcc18
nuoxoxo <nuxu@e1r3p7.clusters.42paris.fr>
2023-02-16 08:01:38 +0100

    upd. 104 . bfs in cc

--- leet_BFS_0104_max_depth_btree.cc
+++ leet_BFS_0104_max_depth_btree.cc
@@ -1,30 +1,38 @@
-/// BFS
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int maxDepth(TreeNode* root)
     {
-        if (!root)
-            return (0) ;
-
-        deque<TreeNode*>    E = { root };
-        int                 depth = 0;
-
-        while (!E.empty())
+        int res = 0;
+        if (! root)
+            return res;
+        deque<TreeNode *>   D{root};
+        // cout << D.size() << endl;
+        while (!D.empty())
         {
-            int i = -1, len = E.size() ;
-            while (++i < len)
+            int         len = D.size();
+            while (--len > -1)
             {
-                TreeNode*   temp = E.front() ;
+                TreeNode    *node = D.front();
 
-                E.pop_front() ;
-                if (temp->left)
-                    E.push_back(temp->left) ;
-                if (temp->right)
-                    E.push_back(temp->right);
+                D.pop_front();
+                if (node->left)
+                    D.push_back(node->left);
+                if (node->right)
+                    D.push_back(node->right);
             }
-            ++ depth ;
+            ++res;
         }
-        return depth ;
+        return res;
     }
 };
