6422a995317ac1e558aeb894c3eb722284ba3566
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-17 21:09:52 +0100

    mlxxi . py & cc in 2 ways

---
+++ leet_1071_gcd_string___GCDimplt.cc
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return "";
+        int len1 = str1.length(), len2 = str2.length();
+        string  s = len1 > len2? str1: str2;
+        string  res;
+        int     L = GCD(len1, len2);
+        int     i = -1;
+        while (++i < L)
+            res += s[i];
+        return res;
+    }
+
+    int GCD(int a, int b)
+    {
+        int res = min(a, b);
+        while (res)
+        {
+            if (!(a % res) && !(b % res))
+                break ;
+            --res;
+        }
+        return res ;
+    }
+};
--- leet_1071_gcd_string___GCDinblt.py
+++ leet_1071_gcd_string___GCDinblt.py
@@ -1,5 +1,12 @@
 class Solution:
     def gcdOfStrings(self, s1: str, s2: str) -> str:
+        def GCD(a: int, b: int) -> int:
+            res = min(a, b)
+            while res > 0:
+                if a % res == 0 and b % res == 0:
+                    break
+                res -= 1
+            return res
         if s1 + s2 != s2 + s1:
             return ''
         size1, size2 = len(s1), len(s2)
