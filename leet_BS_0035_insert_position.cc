1294a96d9ac627b2256f9fab0c43ba825172778f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-02-20 08:23:13 +0100

    2 ways in cc

--- leet_BS_0035_insert_position.cc
+++ leet_BS_0035_insert_position.cc
@@ -2,17 +2,17 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target)
     {
-        int R = nums.size() - 1, L = 0, mid;
-
+        int L = 0;
+        int R = nums.size() - 1;
         while (L <= R)
         {
-            mid = (R - L) / 2 + L;
-            if (nums[mid] < target)
-                L = mid + 1;
-            if (nums[mid] > target)
-                R = mid - 1;
-            if (nums[mid] == target)
-                return mid;
+            int m = (R - L) / 2 + L;
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] > target)
+                R = m - 1;
+            else
+                L = m + 1;
         }
         return L;
     }
