7fd9931668d90673dcd9b45f7632aae8eae5a361
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-04 21:56:40 +0100

    mmcdxliv . sliding window in ts

---
+++ leet_SLWD_2444_fixed_bound_subarray.ts
@@ -0,0 +1,24 @@
+function countSubarrays(nums: number[], minK: number, maxK: number): number {
+    let res = 0
+    let L = 0, R = 0
+    let foundL = false, foundR = false
+    let start = 0, i = -1
+    while (++i < nums.length) {
+        if (nums[i] < minK || nums[i] > maxK) {
+            foundL = foundR = false
+            start = i + 1
+        }
+        if (nums[i] == minK) {
+            foundL = true
+            L = i
+        }
+        if (nums[i] == maxK) {
+            foundR = true
+            R = i
+        }
+        if (foundL && foundR) {
+            res += Math.min(L, R) - start + 1
+        }
+    }
+    return res 
+};
