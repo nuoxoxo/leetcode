cb193037113324e292944d99152c5637c029ed3c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-07 10:36:25 +0100

    + mmclxxxvii . bs with unsigned long long

---
+++ leet_BS_2187_min_time.cc
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ull unsigned long long
+
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        ull r = (ull) *min_element(time.begin(), time.end()) * (ull) totalTrips;
+        ull l = 0;
+        while (l < r)
+        {
+            ull mid = (r + l) / 2;
+
+            if (ok(mid, time, totalTrips))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return (long long) l;
+    }
+
+    bool    ok(unsigned long long n, vector<int> & time, int total)
+    {
+        unsigned long long  trips = 0;
+        for (int t: time)
+            trips += n / t;
+        return (trips >= total);
+    }
+};
