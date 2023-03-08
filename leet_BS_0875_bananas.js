4c9f4d7211c4434e25359dc73e9d2efc838b518c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-08 23:44:20 +0100

    dccclxxv . js ts

---
+++ leet_BS_0875_bananas.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+
+var minEatingSpeed = function(piles, h) {
+    piles.sort((a, b) => a - b)
+    let M = piles[piles.length - 1]
+    let L = 1
+    let R = M
+    let res = M
+    while (L < R) {
+        let hours = 0
+        let mid = Math.floor((R - L) / 2) + L
+        for (let p of piles) {
+            hours += Math.ceil(p / mid)
+        }
+        if (hours > h) {
+            L = mid + 1
+        } else {
+            res = Math.min(res, mid)
+            R = mid
+        }
+    }
+    return res
+};
