a0eac04416adc83b9956cc3a9832df6a38349a02
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-03-09 09:43:52 +0100

    cxli . uopdate 2 pointers soln CC

--- leet_LIST_0141_has_cycle.cc
+++ leet_LIST_0141_has_cycle.cc
@@ -1,38 +1,3 @@
-/* ******************************************** */
-/*                                              */
-/*              \\             /`/``            */
-/*              ~\o o_       0 0\               */
-/*              / \__)      (u  ); _  _         */
-/*       / \/ \/  /           \  \/ \/ \        */
-/*      /(   . . )            (         )\      */
-/*     /  \_____/              \_______/  \     */
-/*         []  []               [[] [[]    *.   */
-/*         []] []]              [[] [[]         */
-/*                                              */
-/* ************************************ nuo *** */
-
-//  hare and tortoise method cf. #876 middle of list
-
-class Solution {
-public:
-    bool hasCycle(ListNode *head)
-    {
-        ListNode    *H, *T;
-
-        if (!head || !head->next)
-            return false ;
-        H = T = head;
-        while (H->next && H->next->next)
-        {
-            H = H->next->next;
-            T = T->next;
-            if (H == T)
-                return true ;
-        }
-        return false ;
-    }
-};
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -41,3 +6,23 @@ public:
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+class Solution {
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode    *fast, *slow;
+
+        if (!head)
+            return (0);
+        fast = head;
+        slow = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
