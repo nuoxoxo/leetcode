713678461d5c6d1434acb113503a2ad118914cf9
uxn <malcolm.nuo.xu@gmail.com>
2023-03-16 18:30:35 +0100

    cxxix : added py

---
+++ leet_BTREE_0129_sum.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def helper(node: Optional[TreeNode], curr: int) -> int:
+            if not node:
+                return 0
+            res = 10 * curr + node.val
+            if not node.left and not node.right:
+                return res
+            return helper(node.left, res) + helper(node.right, res)
+        return helper(root, 0)
