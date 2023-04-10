05b6d707568e45ab6e9114b30897f6bfb2600fdd
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-10 20:50:10 +0200

    mmcccxv . edit

--- leet_2315_split.py
+++ leet_2315_split.py
@@ -1,7 +1,7 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
         a = s.split('|')
-        print(a)
+        # print(a)
         r = 0
         for i in range(0, len(a), 2):
             r += a[i].count('*')
