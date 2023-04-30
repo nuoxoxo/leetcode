ad3ca80891e73e00cfb6f1d66341e9e4c47726b1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-30 11:01:07 +0200

    xliii . update py - 2 ways

--- leet_0043_multiply_strings.py
+++ leet_0043_multiply_strings.py
@@ -2,22 +2,33 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == '0' or num2 == '0':
             return '0'
-        n1 = len(num1)
-        n2 = len(num2)
-        z = ord('0')
-        prod = [0] * (n1 + n2)
+        len1 = len(num1)
+        len2 = len(num2)
+        zero = ord('0')
+        p = [0] * (len1 + len2)
         res = ''
-        for i in range(n1 - 1, -1, -1):
-            for j in range(n2 - 1, -1, -1):
-                a = ord(num1[i]) - z
-                b = ord(num2[j]) - z
-                p = a * b + prod[i + j + 1]
-                prod[i + j] = p // 10 + prod[i + j]
-                prod[i + j + 1] = p % 10
+        for i in range(len1 - 1, -1, -1):
+            for j in range(len2 - 1, -1, -1):
+                
+                # way 1
+                L = ord(num1[i]) - zero
+                R = ord(num2[j]) - zero
+                temp = L * R + p[i + j + 1]
+                p[i + j] = temp // 10 + p[i + j]
+                p[i + j + 1] = temp % 10 
+
+                # way 2
+                """
+                L = i + j
+                R = i + j + 1
+                tmp = (ord(num1[i]) - zero) * (ord(num2[j]) - zero) + p[R]
+                p[L] = tmp // 10 + p[L]
+                p[R] = tmp % 10
+                """
         i = 0
-        while prod[i] == 0:
+        while p[i] == 0:
             i += 1
-        while i < n1 + n2:
-            res += str(prod[i])
+        while i < len1 + len2:
+            res += str(p[i])
             i += 1
         return res
