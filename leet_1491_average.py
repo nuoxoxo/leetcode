adcd0122a7bc99db4afd919830ac8a5b878f8b7a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-01 11:19:05 +0200

    mcdxci . py

--- leet_1491_average.py
+++ leet_1491_average.py
@@ -1,3 +1,15 @@
 class Solution:
-    def average(self, s: List[int]) -> float:
-        return (sum(s) - max(s) - min(s)) / (len(s) - 2)
+    def average(self, salary: List[int]) -> float:
+        # way 1
+        """
+        hi = max(salary)
+        lo = min(salary)
+        a = []
+        for n in salary:
+            if n not in (lo, hi):
+                a.append(n)
+        return sum(a) / len(a)
+        """
+
+        # way 2
+        return (sum(salary) - max(salary) - min(salary)) / (len(salary) - 2)
