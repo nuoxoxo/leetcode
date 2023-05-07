8e9982415312d8927c82fdf5f5683565262c6705
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-08 00:05:58 +0200

    ccc . DP classic - longest increasing subseq Go

--- leet_DPBS_0300_LIS.go
+++ leet_DPBS_0300_LIS.go
@@ -1,22 +1,22 @@
 func lengthOfLIS(nums []int) int {
-    temp := [] int {}
-	for _, n := range nums {
-        size := len(temp)
-		if size == 0 || temp[size - 1] < n {
-			temp = append(temp, n)
-		} else {
-			L := 0
-			R := size - 1
-			for L < R {
-				mid := (R - L) / 2 + L
-				if temp[mid] < n {
-					L = mid + 1
-				} else {
-					R = mid
-				}
-			}
-			temp[R] = n
-		}
-	}
-	return len(temp)
+    dp := make([]int, len(nums))
+    for i := 0; i < len(nums); i++ {
+        dp[i] = 1
+    }
+    for i := len(nums) - 1; i > -1; i-- {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i] < nums[j] {
+                if dp[i] < dp[j] + 1 {
+                    dp[i] = dp[j] + 1
+                }
+            }
+        }
+    }
+    res := 0
+    for i, n := range dp {
+        if i == 0 || n > res {
+            res = n
+        }
+    }
+    return res
 }
