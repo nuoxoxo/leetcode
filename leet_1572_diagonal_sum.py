44ad80bd68003f098456c32b9aaaff9af50a6d11
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-08 11:07:33 +0200

    mdlxxii . update py, one pass

--- leet_1572_diagonal_sum.py
+++ leet_1572_diagonal_sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        r = len(mat)
-        if r is 0:
-            return mat[0][0]
-        t = 0
-        for i in range(r):
-            for j in range(r):
-                if i is j or r - i is j + 1:
-                    t += mat[i][j]
-        return t
+        r = 0
+        L = len(mat)
+        for i in range(L):
+            r += mat[i][i] + mat[i][L - i - 1]
+        if L % 2 == 1:
+            r -= mat[L // 2][L // 2]
+        return r
