d59da18af1b3b8a8e2584ffc93f38559c0184834
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-08 11:00:31 +0200

    mdlxxii . update cc to one pass soln

--- leet_1572_diagonal_sum.cc
+++ leet_1572_diagonal_sum.cc
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    int diagonalSum(vector<vector<int>>& mat)
-    {
-        int r = mat.size(); 
-        if (r == 1) return mat[0][0];
-
-        int res = 0, i = -1, j;
-        while (++i < r)
-        {
-            j = -1;
-            while (++j < r)
-                if (j == i || j == r - 1 - i)
-                    res += mat[i][j];
-        }
-        return res ;
+    int diagonalSum(vector<vector<int>>& mat) {
+        int len = (int) mat.size();
+        int i = -1;
+        int res = 0;
+        while (++i < len)
+            res += mat[i][i] + mat[i][len - i - 1];
+        if (len % 2)
+            res -= mat[len / 2][len / 2];
+        return res;
     }
 };
