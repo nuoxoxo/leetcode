04aeb190541fd2636ca9fdc0deb475348f2a8303
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-08 11:03:22 +0200

    mdlxxii . update Ts to one pass soln

--- leet_1572_diagonal_sum.ts
+++ leet_1572_diagonal_sum.ts
@@ -1,12 +1,26 @@
 function diagonalSum(mat: number[][]): number {
     let i = -1, j
     let res = 0
-    while (++i < mat.length) {
-        console.log(mat[i][i], mat[i][mat.length - i - 1])
-        res += mat[i][i] + mat[i][mat.length - i - 1]
-        if (i == mat.length - i - 1) {
+    let len: number = mat.length
+    while (++i < len) {
+        console.log(mat[i][i], mat[i][len - i - 1])
+        res += mat[i][i] + mat[i][len - i - 1]
+
+        // way 1
+
+        /*
+        if (i == len - i - 1) {
             res -= mat[i][i]
         }
+        */
     }
+
+    // way 2
+
+    if (len % 2) {
+        let mid = Math.floor(len / 2)
+        res -= mat[mid][mid]
+    }
+
     return res
 };
