2502b54665a89b67db8e1e69fb8c725edd9ad203
uxn <malcolm.nuo.xu@gmail.com>
2023-05-11 08:10:24 +0200

    2629 . + reduce solution

--- JS_2629_compose_functions.ts
+++ JS_2629_compose_functions.ts
@@ -1,5 +1,22 @@
 type F = (x: number) => number;
 
+// way 2: reduce
+
+function compose(functions: F[]): F {
+	return function(x) {
+        let init: number = x
+        let res: number = x
+        functions.reverse()
+        return functions.reduce(
+            (res, fn, index) => res = fn(res), // callbackFn
+            init //initialValue
+        )
+    }
+};
+
+// way 1: iterative
+
+/*
 function compose(functions: F[]): F {
 	return function(x) {
         let i = functions.length
@@ -9,6 +26,7 @@ function compose(functions: F[]): F {
         return x
     }
 };
+*/
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
