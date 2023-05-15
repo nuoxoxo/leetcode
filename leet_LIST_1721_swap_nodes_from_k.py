6ca1cfb20a009b57383cca9de50e5e115f827fc5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-05-15 22:27:36 +0200

    mdccxxi . swap nodes clever way . soln 2023

--- leet_LIST_1721_swap_nodes_from_k.py
+++ leet_LIST_1721_swap_nodes_from_k.py
@@ -4,6 +4,26 @@
 #         self.val = val
 #         self.next = next
 
+
+# solved again in 2023
+
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        p = head
+
+        while p and k > 1:
+            p = p.next
+            # i += 1
+            k -= 1
+        from_left = p
+        from_right = head
+        while p.next:
+            from_right = from_right.next
+            p = p.next
+        from_left.val, from_right.val = from_right.val, from_left.val
+        return head
+
+"""
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         lend = head
@@ -20,3 +40,4 @@ class Solution:
         lend.val = rend.val
         rend.val = n
         return head
+"""
