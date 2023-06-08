cbf2c74e17375142a57260ca078033886f62e76f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-08 09:40:02 +0200

    mmccclxxxix . go using prefix sum

---
+++ leet_PREFIX_2389_longest_subseq.go
@@ -0,0 +1,25 @@
+func answerQueries(nums []int, queries []int) []int {
+    sort.Ints(nums)
+    n := len(nums)
+    m := len(queries)
+    prefix := 0
+    var pref []int
+    res := make([]int, m) // auto-filled w/ 0
+    for _, n := range nums {
+        prefix += n
+        pref = append(pref, prefix)
+    }
+    for i, q := range queries {
+        tmp := 0
+        j := 0
+        for j < n {
+            if pref[j] > q {
+                break
+            }
+            tmp++
+            j++
+        }
+        res[i] = tmp
+    }
+    return res
+}
