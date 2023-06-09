08b7a979fbc0d25453744d9cdd4c6b0f8950df9c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-09 21:44:58 +0200

    0744 - + bs soln

---
+++ leet_BS_0744_smallest_letter.ts
@@ -0,0 +1,15 @@
+function nextGreatestLetter(letters: string[], target: string): string {
+    let n: number = letters.length
+    let L:number = 0
+    let R:number = n
+    while (L < R) {
+        let mid:number = Math.floor((R - L) / 2) + L
+        if (letters[mid].charCodeAt(0) <= target.charCodeAt(0))
+            L = mid + 1
+        else
+            R = mid
+    }
+    if (R == n)
+        return letters[0]
+    return letters[R]
+};
