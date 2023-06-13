35ea75bb1db14def0a88371e5bf9e528a49c92cf
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-13 10:58:25 +0200

    + 2352 . edit cc

--- leet_2352_equal_pairs.cc
+++ leet_2352_equal_pairs.cc
@@ -11,12 +11,14 @@ public:
         for (vector<int> nums: grid)
         {
             string  s;
+            string  tmp;
             int     i = -1;
 
             while (++i < len)
             {
-                s += ' ' + to_string(nums[i]);
-                cols[i] += ' ' + to_string(nums[i]);
+                tmp = to_string(nums[i]);
+                s += ' ' + tmp;
+                cols[i] += ' ' + tmp;
             }
             ++dict[s];
         }
