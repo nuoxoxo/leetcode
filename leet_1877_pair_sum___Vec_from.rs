1ab679a5bc30939adb64820594bcd4176945c6eb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-13 22:49:23 +0200

    mdccclxxvii . + rust using Vec::from

---
+++ leet_1877_pair_sum___Vec_from.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn min_pair_sum(nums: Vec<i32>) -> i32 {
+        let mut n = Vec::from(nums);
+        // let mut n = nums.to_owned();
+        // let mut n = nums.to_vec();
+
+        n.sort();
+        let len = n.len();
+        let mut res = std::i32::MIN;
+        let mut i = 0;
+        while i < len {
+            res = res.max(n[i] + n[len - i - 1]);
+            i += 1;
+        }
+        res
+    }
+}
