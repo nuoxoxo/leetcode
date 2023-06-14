c48a39183ee80f3898ac4076812f45afd4d11b82
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-14 09:44:38 +0200

    530 : bfs in py

---
+++ leet_BFS_BST_0530_distance.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        D = [root]
+        n = []
+        while D:
+            node = D.pop(0)
+            n.append(node.val)
+            if node.left:
+                D.append(node.left)
+            if node.right:
+                D.append(node.right)
+        n.sort()
+        res = 1e9
+        for i in range(1, len(n)):
+            tmp = n[i] - n[i - 1]
+            res = min(res, tmp)
+        return res
