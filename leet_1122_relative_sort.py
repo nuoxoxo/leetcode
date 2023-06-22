13c756d02fed18b79b2bed6dd9b0e1ab8c1b34f8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-22 20:08:19 +0200

    1122 . defaultdict

---
+++ leet_1122_relative_sort.py
@@ -0,0 +1,16 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        D = defaultdict()
+        tail = []
+        res = []
+        for n in arr1:
+            if n not in arr2:
+                tail.append(n)
+        tail.sort()
+        for n in arr2:
+            D[n] = arr1.count(n)
+        # print(D)
+        for l, r in D.items():
+            res += [l] * r
+            print(res)
+        return res + tail
