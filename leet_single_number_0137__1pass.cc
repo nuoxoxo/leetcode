b59e4e442d7fb8006c61742461d744b88df4cab9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-04 08:41:55 +0200

    cxxxvii : update 137

---
+++ leet_single_number_0137__1pass.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        int     i;
+
+        if (nums.size() == 1)
+            return (nums[0]);
+        sort(nums.begin(), nums.end());
+        if (nums[0] != nums[1])
+            return (nums[0]);
+        i = -1;
+        while (++i < nums.size() - 2)
+        {
+            if (nums[i] != nums[i + 1] && nums[i + 1] != nums[i + 2] )
+                return (nums[i + 1]);
+        }
+        return nums[nums.size() - 1];
+    }
+};;
