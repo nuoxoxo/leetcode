d1bd9b775f18ec8dfa8d7213cfa21200aadf5b9b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-05 09:30:48 +0200

    cxxxvii : corrected again, count of ones and ret

--- leet_SLWD_1493_longest_ones.py
+++ leet_SLWD_1493_longest_ones.py
@@ -2,15 +2,12 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         tt = 1
         L = 0
-        R = 0
-        while R < len(nums):
+        for R in range(len(nums)):
             if nums[R] == 0:
                 tt -= 1
             if tt < 0:
-                print(tt)
                 if nums[L] == 0:
                     tt += 1
                 L += 1
-            R += 1
-        return R - L - 1
+        return len(nums) - 1 - L
 
