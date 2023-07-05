27ad81531b332c8ef28af72fbbe6e1318843449d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-05 09:14:47 +0200

    cxxxvii : 1493 prefx sum + sliding window

---
+++ leet_SLWD_1493_longest_ones.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& n)
+    {
+        int tt = 1;
+        int L = 0;
+        int R = -1;
+        while (++R < (int) n.size())
+        {
+            if (n[R] == 0)
+                --tt;
+            if (tt < 0)
+            {
+                if (n[L] == 0)
+                    tt = 0;
+                ++L;
+            }
+        }
+        return R - L - 1;
+    }
+};
