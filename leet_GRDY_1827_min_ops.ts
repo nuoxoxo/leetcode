503f50f52e4b9de365ffb9e6b7580ad0e2a5e267
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-09 21:26:09 +0200

    1827 min ops : greedy keeping track of ones prev elem Ts

---
+++ leet_GRDY_1827_min_ops.ts
@@ -0,0 +1,17 @@
+nction minOperations(nums: number[]): number {
+    let len: number = nums.length
+    let res: number = 0
+    let i: number = 0 // start from 1
+    while (++i < len) {
+
+        if (nums[i - 1] < nums[i]) {// right order
+            continue
+        }
+        // not strictly incr.
+        let need: number = nums[i - 1] - nums[i] + 1
+        res += need
+        nums[i] = nums[i - 1] + 1
+
+    }
+    return res
+};
