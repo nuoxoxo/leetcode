50497a39bbc2c06d40dc0ec3a925959f5d72d8a7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-10 09:11:24 +0200

    1534 : update correctcode

--- leet_1534_count_triplets.ts
+++ leet_1534_count_triplets.ts
@@ -1,19 +1,17 @@
-func flipAndInvertImage(image [][]int) [][]int {
-    res := [][]int {}
-    for _, r := range image {
-        tmp := []int {}
-        rr := r
-        for i, j := 0, len(rr) - 1; i < j; i, j = i + 1, j - 1 {
-            rr[i], rr[j] = rr[j], rr[i]
-        }
-        for _, n := range rr {
-            if n == 1 {
-                tmp = append(tmp, 0)
-            } else {
-                tmp = append(tmp, 1)
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let res = 0
+    let L = arr.length
+    let i = -1;
+    while (++i < L - 2) {
+        let j = i
+        while (++j < L - 1) {
+            let k = j
+            while (++k < L) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) {
+                    res += 1
+                }
             }
         }
-        res = append(res, tmp)
     }
     return res
-}
+};
