55b362a9e719f2e8037b47ff67d824f6466c3a1b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-14 22:26:08 +0200

    1218 : easy solution in go

--- leet_DP_1218_arithmetic_subseq___go2.go
+++ leet_DP_1218_arithmetic_subseq___go2.go
@@ -1,8 +1,8 @@
 func longestSubsequence(arr []int, diff int) int {
     d := make(map[int]int)
     res := 0
-    for i := 0; i < len(arr); i++ {
-        d[arr[i]] = d[arr[i] - diff] + 1
+    for _, n := range arr {
+        d[n] = d[n - diff] + 1
     }
     for _, v := range d {
         if res < v {
