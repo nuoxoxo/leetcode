85113a1d677dafe730e6643885e358a6b71631c8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-19 21:00:45 +0200

    435 : sort by [0] then check overlapping . default sort

--- leet_GRDY_0435_overlapping___sort.cc
+++ leet_GRDY_0435_overlapping___sort.cc
@@ -2,9 +2,14 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& I /* intervals */)
     {
-        sort(I.begin(), I.end(), [](vector<int> & a, vector<int> & b) {
-            return a[0] < b[0];
-        });
+        sort(I.begin(), I.end());
+
+        // UP == DOWN : sort by [0] by default
+
+        // sort(I.begin(), I.end(), [](vector<int> & a, vector<int> & b) {
+        //     return a[0] < b[0];
+        // });
+
         int left = -2147483648;
         int res = 0;
         for (vector<int> & i: I)
