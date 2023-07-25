ebe96bab4d67e76ff7f15e151c83e737c8d44039
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-25 13:46:43 +0200

    852 : binary search in GO

---
+++ leet_BS_0852_peak_of_moutain_array.go
@@ -0,0 +1,15 @@
+func peakIndexInMountainArray(arr []int) int {
+
+	L := 0
+	R := len(arr) - 1
+
+	for L <= R {
+		mid := (R-L+1)/2 + L
+		if arr[mid] < arr[mid+1] {
+			L = mid + 1
+		} else {
+			R = mid - 1
+		}
+	}
+	return L
+}
