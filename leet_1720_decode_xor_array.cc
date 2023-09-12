a5a4d25c6ad48b08d3fb503b25f99680f5419d67
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-12 11:26:27 +0200

    1720 : rename

--- leet_1720_decode_xor_array.cc
+++ leet_1720_decode_xor_array.cc
@@ -3,8 +3,7 @@ public:
     vector<int> decode(vector<int>& encoded, int first)
     {
         vector<int> res = {first};
-
-        for (int n : encoded)
+        for (int n: encoded)
         {
             first ^= n;
             res.push_back(first);
