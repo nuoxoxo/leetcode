2c3a8b842d7ece2402889319ecd1c7ca6376cee7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-12 11:20:42 +0200

    1647 : upd. cc using int array

--- leet_1647_min_delete___intarray.cc
+++ leet_1647_min_delete___intarray.cc
@@ -2,33 +2,21 @@ class Solution {
 public:
     int minDeletions(string s)
     {
-        int e[26], i, pos, len, res;
-
+        int count[26] = {0};
+        int i = -1, res = 0;
         i = -1;
-        while (++i < 26)
-        {
-            e[i] = 0;
-        }
-        len = s.length();
+        while (++i < s.length())
+            ++count[s[i] - 'a'];
         i = -1;
-        while (++i < len)
-        {
-            pos = s[i] - 'a';
-            e[pos]++;
-        }
-        int size = sizeof(e) / sizeof(e[0]);    /// obviously 26 but do this by default 
-        sort(e, e + size);
-        res = 0;
-        i = -1;
+        sort(count, count + 25);
         while (++i < 25)
         {
-            if (e[i] && e[i] == e[i + 1])
-            {
-                e[i] -= 1;
-                res += 1;
-                i = -1;
-            }
+            if (!count[i] || count[i] != count[i + 1])
+                continue ;
+            --count[i];
+            ++res;
+            i = -1;
         }
         return res;
     }
-};
+};;
