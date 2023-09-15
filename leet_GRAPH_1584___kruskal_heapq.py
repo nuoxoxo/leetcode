8bc2cdc45d357f071ac7ec73dd945a5c6debac3c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-15 20:09:43 +0200

    1584 : upd. HeapQ

--- leet_GRAPH_1584___kruskal_heapq.py
+++ leet_GRAPH_1584___kruskal_heapq.py
@@ -1,18 +1,16 @@
-class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def manh_dist(x, y):
-            return abs(x[0] - y[0]) + abs(x[1] - y[1])
-        pq = []
-        tt = 0
-        for i in range(1, len(points)):
-            heapq.heappush(
-                pq,
-                [ manh_dist(points[0], points[i]), points[i] ]
-            )
-        while pq:
-            temp = heapq.heappop(pq)
-            tt += temp[0]
-            for dist in pq:
-                dist[0] = min(dist[0], manh_dist(temp[1], dist[1]))
-            heapq.heapify(pq)
-        return tt
+  def minCostConnectPoints(self, points: List[List[int]]) -> int:
+      def CalMan(L, R): # p1, p2
+          return abs(L[0] - R[0]) + abs(L[1] - R[1])
+      pq = []
+      tt = 0
+      p0 = points.pop(0) # 1 : we simply take the first element as our ref point
+      for p in points:
+          dist = CalMan(p0, p) 
+          heapq.heappush(pq, [dist, p]) # 2 : push the dist btw P0 and Pi to heapQ
+      while pq:
+          dist, coor = heapq.heappop(pq)
+          tt += dist # BFS : recap top of Q which has current minimal dist
+          for p in pq: # HeapQ : update each elem ; update / heapify the Q
+              p[0] = min(p[0], CalMan(p[1], coor))
+          heapq.heapify(pq)
+      return tt
