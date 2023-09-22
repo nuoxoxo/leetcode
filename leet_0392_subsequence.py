c0885d22ece8171926171a42aa841883a0dd62d8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-22 19:19:26 +0200

    392 : update new py

--- leet_0392_subsequence.py
+++ leet_0392_subsequence.py
@@ -1,14 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        size = len(s)
-        if size is 0:
+        if s == '':
             return True
-        if len(t) is 0:
-            return False
-        i = 0
-        for c in t:
-            if s[i] == c:
+        ls = len(s)
+        lt = len(t)
+        i, j = 0, 0
+        while i < ls and j < lt:
+            if s[i] == t[j]:
                 i += 1
-            if size == i:
+            if i == ls:
                 return True
+            j += 1
         return False
+
