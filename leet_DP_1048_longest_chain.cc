fbec8a8cd68fedbcce958dd42aa35d5432561fb0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-23 09:35:15 +0200

    1048 : DP in cc

--- leet_DP_1048_longest_chain.cc
+++ leet_DP_1048_longest_chain.cc
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& arr)
+    int longestStrChain(vector<string>& words)
     {
-        map<string, int>    dp;
+        unordered_map<string, int>  dp;
         int res = 1;
-
-        sort(arr.begin(), arr.end(), [](string &s1, string &s2){
-            return s1.length() < s2.length();
+        sort(words.begin(), words.end(), [](string & a, string & b){
+            return a.length() < b.length();
         });
-
-        for (string s: arr)
+        for (string & w: words)
         {
             int i = -1;
-            while (++i < s.length())
+            while (++i < w.length())
             {
-                string  tmp = s;
-                tmp.erase(i, 1);
-                dp[s] = max(dp[tmp] + 1, dp[s]);
+                string s = w.substr(0, i) + w.substr(i + 1);
+                cout << s << endl;
+                dp[w] = max(dp[w], dp[s] + 1);
             }
-            res = max(res, dp[s]);
+            res = max(res, dp[w]);
         }
         return res;
     }
