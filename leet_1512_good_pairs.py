6250aed31753892e99f40cc496b559ee1c7325d9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-03 21:50:22 +0200

    1512 : + py using defaultdict

--- leet_1512_good_pairs.py
+++ leet_1512_good_pairs.py
@@ -1,9 +1,28 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        n = len(nums)
-        cnt = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] == nums[j]:
-                    cnt += 1  
-        return cnt
+        def count(n: int) -> int:
+            res = 0
+            n -= 1
+            while n > 0:
+                res += n
+                n -= 1
+            return res
+
+        """
+        Way 1: Counter
+        """
+        # d = Counter(nums)
+
+        """
+        # Way 2: defaultdict(int)
+        """
+
+        d = defaultdict(int)
+        for n in nums:
+            d[n] += 1
+        res = 0
+        for _, n in enumerate(d):
+            if d[n] > 1:
+                res += count(d[n])
+        return res
+
