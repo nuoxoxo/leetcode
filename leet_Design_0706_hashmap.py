3b311ce869fae82a7e2a74187bdae42f3b848b03
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-04 21:11:17 +0200

    706 : new py code

--- leet_Design_0706_hashmap.py
+++ leet_Design_0706_hashmap.py
@@ -1,25 +1,26 @@
 class MyHashMap:
 
     def __init__(self):
-        self.k = []
-        self.v = []
+        self.keys = []
+        self.vals = []
 
     def put(self, key: int, value: int) -> None:
-        if key in self.k:
-            self.v[self.k.index(key)] = value
+        if key in self.keys:
+            self.vals[ self.keys.index(key) ] = value
             return
-        self.v.append(value)
-        self.k.append(key)
+        self.vals.append(value)
+        self.keys.append(key)
 
     def get(self, key: int) -> int:
-        if key in self.k:
-            return self.v[self.k.index(key)]
+        if key in self.keys:
+            return self.vals[ self.keys.index(key) ]
         return -1
 
     def remove(self, key: int) -> None:
-        if key in self.k:
-            self.v.pop(self.k.index(key))
-            self.k.remove(key)
+        if key not in self.keys:
+            return
+        self.vals.pop( self.keys.index(key) )
+        self.keys.remove(key)
 
 
 # Your MyHashMap object will be instantiated and called as such:
