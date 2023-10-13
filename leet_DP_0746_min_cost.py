a5906d465cfce1b960b0326a00fa4244eb66174a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-13 10:55:43 +0200

    upd. 746

--- leet_DP_0746_min_cost.py
+++ leet_DP_0746_min_cost.py
@@ -1,29 +1,9 @@
-#* ******************************************** *#
-#*                                              *#
-#*              \\             /`/``            *#
-#*              ~\o o_       0 0\               *#
-#*              / \__)      (u  ); _  _         *#
-#*       / \/ \/  /           \  \/ \/ \        *#
-#*      /(   . . )            (         )\      *#
-#*     /  \_____/              \_______/  \     *#
-#*         []  []               [[] [[]    *.   *#
-#*         []] []]              [[] [[]         *#
-#*                                              *#
-#* ************************************ nuo *** *#
-
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        n = len(cost)
-        
-        dp = [0] * (n + 1)
-        # dp = defaultdict(lambda: 0) # defaultdict is faster
-        
-        i = 2
-        while i <= n:
-            L = dp[i - 2] + cost[i - 2];
-            R = dp[i - 1] + cost[i - 1];
-            dp[i] = L if L < R else R
-            i += 1
-        # print(dp)
-        return dp[n]
+        size = len(cost)
+        dp = [0] * (size + 1)
+        for i in range(2, size + 1, 1):
+            l = dp[i - 2] + cost[i - 2]
+            r = dp[i - 1] + cost[i - 1]
+            dp[i] = l if l < r else r
+        return dp[size]
