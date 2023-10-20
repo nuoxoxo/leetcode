fecc15b31a0514609726fdac5c552d8e92a10c02
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-20 08:57:29 +0200

    341 : upd. ts using private var and func

---
+++ leet_Design_0341_flatten___private.ts
@@ -0,0 +1,71 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+class NestedIterator {
+
+    private _arr: number[] = []
+    private _flatten = (nestedList) => {
+        for (let item of nestedList) {
+            if (item.isInteger()) {
+                this._arr.push(item.getInteger())
+            } else {
+                this._flatten(item.getList()) // this keyword need when recurse 
+            }
+        }
+    }
+
+    constructor(nestedList: NestedInteger[]) {
+        this._flatten(nestedList) 
+    }
+
+    hasNext(): boolean {
+		return this._arr.length > 0
+    }
+
+	next(): number {
+		return this._arr.shift() || 0 // undefined when empty
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new NestedIterator(nestedList)
+ * var a: number[] = []
+ * while (obj.hasNext()) a.push(obj.next());
+ */
