6dac61295aa7100e7f22f59468c852f3ddf4d813
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-26 18:22:52 +0200

    746 : update cc . easy dp

--- leet_DP_0746_min_cost.cc
+++ leet_DP_0746_min_cost.cc
@@ -1,33 +1,20 @@
-/* ******************************************** */
-/*                                              */
-/*              \\             /`/``            */
-/*              ~\o o_       0 0\               */
-/*              / \__)      (u  ); _  _         */
-/*       / \/ \/  /           \  \/ \/ \        */
-/*      /(   . . )            (         )\      */
-/*     /  \_____/              \_______/  \     */
-/*         []  []               [[] [[]    *.   */
-/*         []] []]              [[] [[]         */
-/*                                              */
-/* ************************************ nuo *** */
-
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost)
     {
-        int         L, R, i, n;
+        int L, R, i, n;
+        int size = cost.size();
 
-        n = cost.size() + 1;
-        vector<int> dp(n);
+        vector<int> dp(size + 1);
         dp[0] = 0;
         dp[1] = 0;
         i = 1;
-        while (++i < n)
+        while (++i < size + 1)
         {
             L = dp[i - 2] + cost[i - 2];
             R = dp[i - 1] + cost[i - 1];
             dp[i] = L < R ? L : R;
         }
-        return dp[n - 1] ;
+        return dp[size] ;
     }
-};
+};;
