1fb5bbde7d59e0eace3c8f980ecb35a39922aa64
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-10-29 22:35:46 +0100

    338 : + go .

--- leet_DP_338_count_bits.go
+++ leet_DP_338_count_bits.go
@@ -1,7 +1,7 @@
 func countBits(n int) []int {
-    E := make([]int, n+1)
-    for i := 0; i < n+1; i++ {
-        E[i] = E[i/2] + i%2
+    E := make([]int, n + 1)
+    for i := 0; i < n + 1; i++ {
+        E[i] = E[i / 2] + i % 2
     }
     return E
 };
