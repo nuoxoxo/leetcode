816c1c5ad150f23e3218865995367e9631629b2e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-17 16:58:10 +0100

    1877 : upd simple soln ts

--- leet_1877_pair_sum.ts
+++ leet_1877_pair_sum.ts
@@ -1,4 +1,16 @@
 function minPairSum(nums: number[]): number {
+
+    // new soln
+    let len = nums.length
+    let l = 0, r = len - 1
+    let res = 0
+    nums.sort((a, b) => a - b)
+    while (l < r) {
+        res = Math.max(res, nums[l++] + nums[r--])
+    }
+    return res
+
+    /*
     nums.sort((a, b) => a - b)
     // console.log(nums)
     let len:number = nums.length
@@ -8,4 +20,5 @@ function minPairSum(nums: number[]): number {
         res = Math.max(res, nums[i] + nums[len - i - 1])
     }
     return res
+    */
 };
