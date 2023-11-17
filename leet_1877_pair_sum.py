27e0a2fe745e221a5cd01f6fdbeaf61c9bcad0ac
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-17 16:54:56 +0100

    1877 : upd simple soln py

--- leet_1877_pair_sum.py
+++ leet_1877_pair_sum.py
@@ -1,9 +1,23 @@
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
+
+        # new soln
+
         nums.sort()
-        size = len(nums)
-        mid = size // 2
-        res = -1e9
-        for i in range(mid):
-            res = max(res, nums[i] + nums[size - i - 1])
+        res = 0
+        l, r = 0, len(nums) - 1
+        while l < r:
+            res = max(res, nums[l] + nums[r])
+            l += 1
+            r -= 1
         return res
+
+        # old soln
+
+        # nums.sort()
+        # size = len(nums)
+        # mid = size // 2
+        # res = -1e9
+        # for i in range(mid):
+        #     res = max(res, nums[i] + nums[size - i - 1])
+        # return res
