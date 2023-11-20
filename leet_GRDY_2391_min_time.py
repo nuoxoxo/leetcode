8c87f620b144aa33870fd6f713d019b6fc2766ba
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-20 23:39:17 +0100

    2391 - insight: every garbage type is independent of each other py

---
+++ leet_GRDY_2391_min_time.py
@@ -0,0 +1,22 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        first = garbage[0]
+        gg = first.count('G')
+        mm = first.count('M')
+        pp = first.count('P')
+        G, M, P = gg, mm, pp
+        for i in range(1, len(garbage)):
+            t = travel[i - 1]
+            s = garbage[i]
+            ifg, ifm, ifp = s.count('G'), s.count('M'), s.count('P')
+            gg += t + ifg
+            mm += t + ifm
+            pp += t + ifp
+            ifg, ifm, ifp = s.count('G'), s.count('M'), s.count('P')
+            if ifg > 0:
+                G = gg
+            if ifm > 0:
+                M = mm
+            if ifp > 0:
+                P = pp
+        return G + M + P
