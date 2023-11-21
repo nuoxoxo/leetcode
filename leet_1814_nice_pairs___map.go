76aa2997f1d7d17721d12985b00119ba05aa73ef
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-21 17:21:04 +0100

    1814 : + go

---
+++ leet_1814_nice_pairs___map.go
@@ -0,0 +1,18 @@
+func countNicePairs(nums []int) int {
+    D := make(map[int]int)
+	MOD := int(1e9) + 7
+	for _, n := range nums {
+		original := n
+		res := 0
+		for n > 0 {
+			res = res * 10 + n % 10
+			n /= 10
+		}
+		D[original-res]++
+	}
+	res := 0
+	for _, v := range D {
+		res = (res + v * (v - 1) / 2) % MOD
+	}
+	return res
+}
