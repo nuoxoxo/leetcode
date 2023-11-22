b5170a72225130919e1cfbfba23703f09dae071e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-22 21:58:11 +0100

    1424 : PERKS OF GO - key order is undefined

---
+++ leet_1424_diagonal_traverse_ii.go
@@ -0,0 +1,47 @@
+func findDiagonalOrder(nums [][]int) []int {
+    D := make( map[int][]int )
+    r := 0
+    colmax := 0
+    var c int
+    for r < len(nums) {
+        c = 0
+        for c < len(nums[r]) {
+            D[r + c] = append(D[r + c], nums[r][c])
+            c++
+            colmax = max(colmax, r + c)
+        }
+        r++
+    }
+
+    // perks of go
+    var keys []int
+	for k := range D {
+		keys = append(keys, k)
+	}
+
+    fmt.Println(D)
+    var res []int
+    //  Perks of Go : key order is undefined when traversing a map like this:
+    /*
+    for _, k := range keys {
+        arr := D[k]
+        for len(arr) > 0 {
+            res = append(res, arr[len(arr) - 1])
+            arr = arr[ : len(arr) - 1]
+        }
+    }
+    */
+
+    //  Correct way : keep track of max col and use this as a guide of keys
+    i := 0
+    for i < colmax {
+        arr := D[i]
+        for len(arr) > 0 {
+            res = append(res, arr[len(arr) - 1])
+            arr = arr[ : len(arr) - 1]
+        }
+        i++
+    }
+    return res
+}
+
