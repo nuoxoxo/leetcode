74c9bdb13a112ceb2562e3991aa7fb8d133f7275
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-23 23:36:41 +0100

    125 : simple

--- leet_0125_ispalindrome.py
+++ leet_0125_ispalindrome.py
@@ -1,36 +1,11 @@
-#* ******************************************** *#
-#*                                              *#
-#*              \\             /`/``            *#
-#*              ~\o o_       0 0\               *#
-#*              / \__)      (u  ); _  _         *#
-#*       / \/ \/  /           \  \/ \/ \        *#
-#*      /(   . . )            (         )\      *#
-#*     /  \_____/              \_______/  \     *#
-#*         []  []               [[] [[]    *.   *#
-#*         []] []]              [[] [[]         *#
-#*                                              *#
-#* ************************************ nuo *** *#
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        zero = ord('0')
-        nine = ord('9')
-        A = ord('A')
-        a = ord('a')
-        Z = ord('Z')
-        z = ord('z')
-        ss = ""
+        ss = ''
         for c in s:
-            o = ord(c)
-            if A <= o <= Z:
-                ss += chr(o + 32)
-            if a <= o <= z or zero <= o <= nine:
-                ss += c
-        return self.P(ss)
-            
-    def P(self, s: str) -> bool:
-        L = len(s)
+            if c.isalnum():
+                ss += c.lower()
+        L = len(ss)
         for i in range(L // 2):
-            if s[i] is not s[L - 1 - i]:
+            if ss[i] != ss[L - 1 - i]:
                 return False
         return True
