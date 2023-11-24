b20db398584dba13d5f35ea5d7e967919febae11
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-24 23:15:19 +0100

    1630 : ed. py

--- leet_1630_arithmetic_subarrays.py
+++ leet_1630_arithmetic_subarrays.py
@@ -1,35 +1,21 @@
-func checkArithmeticSubarrays(nums []int, l []int, r []int) []bool {
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        res = []
+        size = len(nums)
+        for i in range(len(l)):
+            # end = min(r[i] + 1, size)
+            # tmp = nums[l[i] : end]
+            tmp = nums[l[i] : r[i] + 1]
+            if len(tmp) < 3:
+                res.append(True)
+                continue
+            tmp.sort()
+            diff = tmp[1] - tmp[0]
+            ok = True
+            for j in range(1, len(tmp)):
+                if tmp[j] - tmp[j - 1] != diff:
+                    ok = False
+                    break
+            res.append(ok)
+        return res
 
-    var res []bool
-    for i := 0; i < len(l); i++ {
-
-        //  Wrong way
-        // sort.Ints(nums)
-
-        // Correct way 1
-        tmp := append([]int(nil), nums[l[i] : r[i] + 1] ...)
-
-        // Correct way 2
-        // tmp := make([]int, len(nums[l[i] : r[i] + 1]))
-        // copy(tmp, nums[l[i] : r[i] + 1])
-
-        len := len(tmp)
-        if len < 3 {
-            res = append(res, true)
-            continue
-        }
-        sort.Ints(tmp)
-        k := tmp[1] - tmp[0]
-        ok := true
-        j := 2
-        for j < len {
-            if tmp[j] - tmp[j - 1] != k {
-                ok = false
-                break
-            }
-            j++
-        }
-        res = append(res, ok)
-    }
-    return res
-}
