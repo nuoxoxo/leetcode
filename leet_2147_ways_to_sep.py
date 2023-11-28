7d61cf80edfbfbef90927fbae6e84d6a6a7578f0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-28 20:11:02 +0100

    2147 : Math & logic, linear way no Dp . mod py

--- leet_2147_ways_to_sep.py
+++ leet_2147_ways_to_sep.py
@@ -1,24 +1,24 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
         mod = int(1e9 + 7)
-        putsep = 0 # can put a sep here : a sep placed represents a factor for all prev arrangements
-        ss = 0 # seat count
+        pls = 0 # a sep placed represents a factor for all prev arrangements
+        sea = 0 # seat count
         res = 1
         L = len(corridor)
-        for seat in corridor:
-            if seat == 'P':
-                if ss > 0 and ss % 2 == 0:
-                    putsep += 1
-            elif seat == 'S':
+        for c in corridor:
+            if c == 'P':
+                if sea > 0 and sea % 2 == 0:
+                    pls += 1
+            elif c == 'S':
                 # either no seat has yet been seen then res is 0
                 # or all seen arrangements should *= potential future arrangements
-                res *= putsep + 1
+                res *= pls + 1
                 res %= mod
                 # ?? modulo is strange
                 # res *= ((putsep % mod + 1) % mod) # ??
-                putsep = 0 # encounter w/ seat stops the count 
-                ss += 1
-        if ss < 1 or ss % 2 != 0:
+                pls = 0 # encounter w/ seat stops the count 
+                sea += 1
+        if sea < 1 or sea % 2 != 0:
             return 0
         return res
 """
