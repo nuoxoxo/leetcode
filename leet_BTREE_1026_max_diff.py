019f73db8f61c532b907131bb7716271e1fb7cfd
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-11 11:46:05 +0100

    upd. 1026 - DFS w/o abs

--- leet_BTREE_1026_max_diff.py
+++ leet_BTREE_1026_max_diff.py
@@ -1,3 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def finder (node: TreeNode, L: int, R: int) -> int :
+            if not node:
+                return R - L
+            L = min(L, node.val)
+            R = max(R, node.val)
+            return int(max( finder(node.left, L, R), finder(node.right, L, R)))
+        return finder (root, 1e9, -1e9)
+
+"""
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         def solve(node: TreeNode, Mx: int, Mn: int) -> int:
@@ -11,11 +28,5 @@ class Solution:
         if not root:
             return 0
         return solve(root, root.val, root.val)
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""
 
