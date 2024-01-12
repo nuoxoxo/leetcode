982520a338bb24a04d6708972c123b98b24e5eaf
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-12 10:17:35 +0100

    upd. 1026 - present BFS/DFS at the same time in CC

--- leet_BTREE_1026_max_diff.cc
+++ leet_BTREE_1026_max_diff.cc
@@ -1,31 +1,3 @@
-class Solution {
-public:
-	int maxAncestorDiff(TreeNode *root)
-	{
-		if (!root)
-		{
-			return 0;
-		}
-		return solve(root, root->val, root->val);
-	}
-
-	int solve(TreeNode *root, int most, int least)
-	{
-		int	L, R;
-
-		if (!root)
-		{
-			return (most - least);
-		}
-		most = max(most, root->val);
-		least = min(least, root->val);
-		L = solve(root->left, most, least);
-		R = solve(root->right, most, least);
-
-		return max(L, R);
-	}
-};
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -37,3 +9,42 @@ public:
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        int n = std::rand() % 2;
+        cout << (n ? "BFS" : "DFS") << endl;
+        return n ? BFS(root, root->val, root->val) : DFS(root, root->val, root->val);
+    }
+    // BFS
+    int BFS(TreeNode * node, int L, int R) {
+        std::deque<std::tuple< TreeNode *, int, int>> D{ {node, L, R} };
+        int res = 0;
+        while ( ! D.empty() ) {
+            std::tuple<TreeNode *, int, int> tpl = D.front();
+            D.pop_front();
+            node = get<0>(tpl);
+            L = get<1>(tpl);
+            R = get<2>(tpl);
+            L = min(L, node->val);
+            R = max(R, node->val);
+            if (node->left)
+                D.push_back( {node->left, L, R} );
+            if (node->right)
+                D.push_back( {node->right, L, R} );
+            res = max(res, R - L);
+        }
+        return res;
+    }
+    // DFS
+    int DFS (TreeNode * node, int L, int R) {
+        if ( ! node)
+            return R - L;
+        L = min(L, node->val);
+        R = max(R, node->val);
+        return max(
+            DFS(node->left, L, R),
+            DFS(node->right, L, R)
+        );
+    }
+};
