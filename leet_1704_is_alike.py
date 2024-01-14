06fcad284c9589e4f4706e48c8b4293bf67b54d1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-14 13:00:21 +0100

    + 1347 : counting . 2 random methods

--- leet_1704_is_alike.py
+++ leet_1704_is_alike.py
@@ -1,6 +1,6 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-
+        charset = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         def Solution_2pass (s: str, charset: list) -> int:
             res = 0
             mid = len(s) // 2
@@ -8,13 +8,11 @@ class Solution:
                 if s[i] in charset: res += 1
                 if s[mid + i] in charset: res -= 1
             return res == 0
-
-        def Solution_cmp_length (s: str, charset: list) -> int:
+        def Solution_list_comprehension (s: str, charset: list) -> int:
             return len([_ for _ in list(s[:len(s)//2]) if _ in charset])\
                 == len([_ for _ in list(s[len(s)//2:]) if _ in charset])
 
-        charset = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         n = random.randint(0, 1)
-        print([ 'Solution_2pass', 'Solution_cmp_length' ][ n ])
+        print([ 'Solution_2pass', 'Solution_list_comprehension' ][ n ])
 
-        return [Solution_2pass, Solution_cmp_length][ n ](s, charset)
+        return [Solution_2pass, Solution_list_comprehension][ n ](s, charset)
