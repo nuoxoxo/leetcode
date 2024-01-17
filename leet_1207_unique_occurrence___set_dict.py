027d3307d9beee8eafdd9bd016daaabf6fbf4247
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-17 11:02:30 +0100

    del. 1207 old py

--- leet_1207_unique_occurrence___set_dict.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        D = defaultdict(lambda: 0)
-        S = set()
-        for n in arr:
-            D[n] += 1
-        for n in arr:
-            S.add(D[n])
-        return len(S) == len(D)
