26208dfe0e47ef3d2422cfc427d133b77fd19b8b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-19 08:55:40 +0100

    + 931 . two solns: in-place, extra space

---
+++ leet_DP_0931_min_path.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        return [
+            self.Solution_DP_extra_matrix,
+            self.Solution_DP_in_place,
+        ][ random.randint(1,1) ]( matrix )
+
+    # T( R x C ), S( 1 )
+    def Solution_DP_in_place(self, matrix: List[List[int]]) -> int:
+        R, C = len(matrix), len(matrix[0])
+        for r in range(1, R):
+            for c in range(C):
+                temp = []
+                for dc in range(-1, 2):
+                    cc = c + dc
+                    if -1 < cc < C:
+                        temp.append(matrix[r - 1][cc] + matrix[r][c])
+                matrix[r][c] = min( temp )
+            # print('/row '+str(r),matrix[r])
+        return min(matrix[-1])
+
+    # T( R x C ), S( R x C )
+    def Solution_DP_extra_matrix(self, matrix: List[List[int]]) -> int:
+        print('\Solution_DP_extra_matrix')
+        R, C = len(matrix), len(matrix[0])
+        DP = [[1e9 for _ in range(C)] for _ in range(R)]
+        DP[0] = matrix[0].copy()
+        for r in range(1, R):
+            for c in range(C):
+                for dc in range(-1, 2):
+                    cc = c + dc
+                    if -1 < cc < C:
+                        DP[r][c] = min(DP[r - 1][cc] + matrix[r][c], DP[r][c])
+        return min(DP[-1])
