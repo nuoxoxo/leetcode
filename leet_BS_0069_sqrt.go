e1929f1f4ae9485e2baf8d2e3c878ea3beef4187
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-22 19:09:46 +0100

    69/go : all solns

---
+++ leet_BS_0069_sqrt.go
@@ -0,0 +1,52 @@
+func mySqrt(x int) int {
+    Solutions := []func (int) int {
+        Solution_Binary_Search,
+        Solution_Newton,
+        Solution_Bruteforce,
+    }
+    return Solutions[rand.Intn(len(Solutions))]( x )
+}
+
+func Solution_Binary_Search(x int) int {
+    fmt.Println("/Solution_Binary_Search")
+    res, L, R := 0, 0, x
+    for L <= R {
+        mid := (R - L) / 2 + L
+        sq := mid * mid
+        if sq == x {
+            return mid
+        }
+        if sq > x {
+            R = mid - 1
+        } else {
+            res = mid
+            L = mid + 1
+        }
+    }
+    return res
+}
+
+func Solution_Newton(x int) int {
+    fmt.Println("/Solution_Newton")
+    if x == 0 {
+        return 0
+    }
+    res := x
+    for res * res > x {
+        res = (res + x / res) / 2
+    }
+    return res
+}
+
+func Solution_Bruteforce(x int)int{
+    fmt.Println("/Solution_Bruteforce")
+    if x == 0 {
+        return x
+    }
+    res := 1
+    for res * res <= x && (res+1) * (res+1) <= x {
+        res += 1
+    }
+    return res
+}
+
