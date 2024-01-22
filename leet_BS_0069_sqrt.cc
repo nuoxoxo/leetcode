f1c4ca39a79ed715b333f823444286bbff27ae62
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-22 20:17:04 +0100

    69/cc : 3 solns

--- leet_BS_0069_sqrt.cc
+++ leet_BS_0069_sqrt.cc
@@ -1,40 +1,49 @@
-/* ******************************************** */
-/*                                              */
-/*              \\             /`/``            */
-/*              ~\o o_       0 0\               */
-/*              / \__)      (u  ); _  _         */
-/*       / \/ \/  /           \  \/ \/ \        */
-/*      /(   . . )            (         )\      */
-/*     /  \_____/              \_______/  \     */
-/*         []  []               [[] [[]    *.   */
-/*         []] []]              [[] [[]         */
-/*                                              */
-/* ************************************ nuo *** */
-
 class Solution {
 public:
-    int mySqrt(int x)
-    {
-        long    tmp, res, mp;
-        int     L, R;
-
-        mp = x / 2;
-        L = 0;
-        R = x;
+    int mySqrt(int x) {
+        vector<int(*)(int)> Solutions {
+            Solution_Binary_Search,
+            Solution_Newton,
+            Solution_Brute_Force,
+        };
+        return Solutions[ std::rand() % Solutions.size() ]( x );
+    }
+    // Binary search
+    static int Solution_Binary_Search (int x) {
+        cout << "/Solution_Binary_Search\n";
+        if (x == 0 || x == 1) return x;
+        long long L = 0, R = x / 2, res = 1;
         while (L <= R)
         {
-            tmp = mp * mp;
-            if (tmp == x)
-                return mp ;
-            if (tmp > x)
-                R = mp - 1;
-            if (tmp < x)
-            {
-                res = mp;
-                L = mp + 1;
+            long long mid = (R-L)/2 + L;
+            long long sq = mid * mid;
+            if (sq == x) return mid;
+            if (sq > x) {
+                R = mid - 1;
+            } else {
+                res = mid;
+                L = mid + 1;
             }
-            mp = (R - L) / 2 + L ;
         }
-        return res ;
+        return res;
+    }
+    // Newton's solution
+    static int Solution_Newton (int x) {
+        cout << "/Solution_Newton\n";
+        if (x == 0 || x == 1) return x;
+        long long res = x;
+        while (res * res > x) {
+            res = (res + x / res) / 2; // Newton's formula
+        }
+        return res;
+    }
+    // Brute force
+    static int Solution_Brute_Force (int x) {
+        cout << "/Solution_Brute_Force\n";
+        if (x == 0 || x == 1) return x;
+        long long res = 1;
+        while (res * res <= x && (res+1) * (res+1) <= x)
+            ++res;
+        return res;
     }
 };
