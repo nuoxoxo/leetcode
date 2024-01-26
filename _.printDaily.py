47cad530f4be95795c881d25f8ea90f5867ed4a3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-26 16:52:14 +0100

    + print daily script . mod

--- _.printDaily.py
+++ _.printDaily.py
@@ -33,6 +33,12 @@ query = """query getDailyChallenge {
   }
 }"""
 
+GR = '\033[92m'
+YL = '\033[93m'
+MG = '\033[95m'
+CY = '\033[96m'
+RE = '\033[0m'
+
 def printDaily():
     payload = { 'query': query }
     resp = requests.post( endpoint, json=payload )
@@ -42,30 +48,31 @@ def printDaily():
     D = json.loads(resp.text)['data']['activeDailyCodingChallengeQuestion']['question']
     qid = D['questionId']
     name, nameslug = D['title'], D['titleSlug']
-    cate, diff = D['categoryTitle'], D['difficulty']
+    typ_, diff = D['categoryTitle'], D['difficulty']
     love, hate = D['likes'], D['dislikes']
     tops = ', '.join([_['name'] for _ in D['topicTags']])
     sqList = [ _ for _ in D['similarQuestionList']]
 
-    Name = f'{qid} - {name}'
-    Cate = f'type: {cate}'
-    Diff = f'rank: {diff}'
-    Like = f'love: {love}✓:{hate}✖'
-    Tops = f'type: {tops}'
+    Name = f'{qid} - {GR}{name}{RE}'
+    Typ_ = f'Type: {typ_}'
+    Diff = f'Rank: {diff}'
+    Like = f'Love: {love}✓:{hate}✖'
+    Tops = f'Tags: {tops}'
     Hints = '\n\n'.join([ f'{i}: {_}' for i,_ in enumerate( D['hints']) ])
+    print(f'{YL}Daily question{RE}\n')
     print(Name)
     print(Diff)
-    print(Cate)
     print(Tops)
+    print(Typ_)
     print(Like,'\n')
     print(Hints)
     print()
     for i, sq in enumerate(sqList):
-        print('similar question',i + 1)
-        print( f'{sq['questionId']} - {sq['title']}' )
+        print(f'{YL}Similar question{RE} {i + 1}\n')
+        print( f'{sq['questionId']} - {GR}{sq['title']}{RE}' )
         tops = ', '.join([_['name'] for _ in sq ['topicTags']])
-        print( f'type: {tops}' )
-        print( f'rank: {sq['difficulty']}' )
+        print( f'Tags: {tops}' )
+        print( f'Rank: {sq['difficulty']}' )
         print()
 
 printDaily()
