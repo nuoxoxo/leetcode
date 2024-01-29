fb3a5fab9be983fa06c828282996ebaa0b1628c0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-29 08:45:32 +0100

    + 232/cc . using vector - new soln

---
+++ leet_Design_0232_queue___vec.cc
@@ -0,0 +1,38 @@
+class MyQueue {
+
+vector<int> vec;
+
+public:
+    MyQueue() {
+        // left blank
+    }
+    
+    void push(int x) {
+        this->vec.push_back(x);
+    }
+    
+    int pop() {
+        assert( ! this->vec.empty());
+        int res = this->vec[0];
+        this->vec.erase( this->vec.begin() );
+        return res;
+    }
+    
+    int peek() {
+        assert( !this->vec.empty() );
+        return this->vec[0];
+    }
+    
+    bool empty() {
+        return (!this->vec.size());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
