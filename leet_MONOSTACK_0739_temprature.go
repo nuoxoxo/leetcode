df5fadfe69134f8e45de7f19f817ecb3b2226323
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-31 11:38:34 +0100

    - 739/go : monotonic stack . back/forward solns

--- leet_MONOSTACK_0739_temprature.go
+++ leet_MONOSTACK_0739_temprature.go
@@ -1,4 +1,28 @@
-func dailyTemperatures( t []int) []int {
+func dailyTemperatures(temperatures []int) []int {
+    Solutions := []func([]int) []int {
+        Monotonic_stack_backward,
+        // Monotonic_stack_forward,
+    }
+    return Solutions[ rand.Intn(len(Solutions)) ]( temperatures )
+}
+
+func Monotonic_stack_forward (t []int) []int {
+    res := make([]int, len(t))
+    stack := []int{}
+    i := 0
+    for i < len(t) {
+        for len(stack) > 0 && t[ stack[len(stack) - 1] ] < t[i] {
+            res[ stack[len(stack) - 1] ] = i - stack[len(stack) - 1]
+            stack = stack[:len(stack) - 1]
+        }
+        stack = append(stack, i)
+        i++
+    }
+    return res
+}
+
+func Monotonic_stack_backward (t[]int) []int {
+
     res := make([]int, len(t))
     stack := []int{}
     i := len(t) - 1
