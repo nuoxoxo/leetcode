e06ca6dd2f1970df7fb0d3a65aad1bc81eff8b10
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-06 08:46:50 +0100

    49/go : using map[string][]string{} . cleanup

--- leet_0049_anagrams.go
+++ leet_0049_anagrams.go
@@ -1,24 +1,11 @@
 func groupAnagrams(strs []string) [][]string {
     D := map[string][]string{}
     res := [][]string{}
-    /*
-    - sort chars in string in Go
-        s := strings.Split(w, "")
-        sort.Strings(s)
-        return strings.Join(s, "")
-    */
     for _, s := range strs {
         tmp := strings.Split(s, "")
-        sort.Strings(tmp )
+        sort.Strings( tmp )
         word := strings.Join(tmp, "")
         D[word] = append(D[word], s)
-        /* YOU DONT NEED CHECKING */
-        // _, ok := D[word]
-        // if ok {
-        //     D[word] = append(D[word], s)
-        // } else {
-        //     D[word] = []string{s}
-        // }
     }
     for _, v := range D {
         res = append(res, v)
