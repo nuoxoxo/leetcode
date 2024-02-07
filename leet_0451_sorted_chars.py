dbc6323f316579c633aeb9eb37c5ca0a378f62ee
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-07 15:27:27 +0100

    451/py : + oneliner

--- leet_0451_sorted_chars.py
+++ leet_0451_sorted_chars.py
@@ -1,5 +1,14 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        return [
+            self.Oneliner,
+            self.Pre_oneliner,
+        ][ 1 ]( s )
+
+    def Oneliner(self, s: str) -> str:
+        return ''.join([ c * fq for c, fq in dict(sorted(Counter(s).items(), key=lambda x: (x[1], x[0]), reverse=True)).items()])
+
+    def Pre_oneliner(self, s: str) -> str:
         res = ''
         for k,v in dict(sorted(Counter(s).items(), key=lambda x: (x[1], x[0]), reverse=True)).items():
             for _ in range(v):
