a7aee2d65cd17fe9fd85915122424f1766d4a490
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-07 15:43:50 +0100

    451/py : + oneliner using most common

--- leet_0451_sorted_chars.py
+++ leet_0451_sorted_chars.py
@@ -1,9 +1,13 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         return [
+            self.Oneliner_most_common,
             self.Oneliner,
             self.Pre_oneliner,
-        ][ 1 ]( s )
+        ][ 0 ]( s )
+
+    def Oneliner_most_common(self, s: str) -> str:
+        return ''.join([ c * fq for c, fq in Counter(s).most_common()])
 
     def Oneliner(self, s: str) -> str:
         return ''.join([ c * fq for c, fq in dict(sorted(Counter(s).items(), key=lambda x: (x[1], x[0]), reverse=True)).items()])
