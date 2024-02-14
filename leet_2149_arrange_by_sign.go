17624bf97f550d44207fde6d8ca58365b7e5c4c6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-14 12:09:32 +0100

    2149/go + array solution

--- leet_2149_arrange_by_sign.go
+++ leet_2149_arrange_by_sign.go
@@ -1,4 +1,29 @@
 func rearrangeArray(nums []int) []int {
+    Solutions := []func([]int)[]int{
+        // Solution_slice,
+        Solution_array,
+    }
+    return Solutions[0](nums)
+}
+
+func Solution_array (nums []int) []int {
+    N := len(nums)
+    res := make([]int, N)
+    posindex := 0
+    negindex := 1
+    for _, n := range nums {
+        if n < 0 {
+            res[ negindex ] = n
+            negindex += 2
+        } else {
+            res[ posindex ] = n
+            posindex += 2
+        }
+    }
+    return res
+}
+
+func Solution_slice (nums []int) []int {
     var P []int
     var N []int
     for _, n := range nums {
