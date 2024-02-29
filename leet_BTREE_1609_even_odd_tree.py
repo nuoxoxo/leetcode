b3ce7d011c62f8ab94d205c28e250afc2848022e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-29 20:42:29 +0100

    leetcode/ + 1609: bfs in py

---
+++ leet_BTREE_1609_even_odd_tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        return [
+            self.Solution_BFS,
+            # self.Solution_DFS,
+        ][ 0 ]( root )
+
+    def Solution_BFS (self, root: Optional[TreeNode]) -> bool:
+        dq = [root]
+        level = 0
+        while dq:
+            # for node in dq: print(node.val, end=' ')
+            # print()
+            level += 1
+            temp = []
+            prev = 0 #-1e9
+            for node in dq:
+                isOdd = level % 2 == 0
+                # level odd :: strictly increasing order
+                if node.val % 2 == isOdd or \
+                isOdd and prev > 0 and prev <= node.val or \
+                not isOdd and prev >= node.val:
+                # level even .. decreasing
+                    return False
+                prev = node.val
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+                prev = node.val
+            dq = temp
+        return True
+
