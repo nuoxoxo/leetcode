a106c36d8f2989f22c28d9f0582f67eb3b2e99aa
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-07 09:13:01 +0100

    141/go - fast & slow ptrs

--- leet_LIST_0141_has_cycle.go
+++ leet_LIST_0141_has_cycle.go
@@ -9,12 +9,12 @@ func hasCycle(head *ListNode) bool {
     if head == nil {
         return false
     }
-    slow := head
-    fast := head
-    for fast != nil && fast.Next != nil {
-        fast = fast.Next.Next
-        slow = slow.Next
-        if slow == fast {
+    s := head
+    f := head
+    for f != nil && f.Next != nil {
+        f = f.Next.Next
+        s = s.Next
+        if s == f {
             return true
         }
     }
