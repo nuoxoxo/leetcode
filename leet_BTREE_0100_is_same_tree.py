f626c6c2423ead0acf5029d7c26fa334109bbe7c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-07 08:41:07 +0100

    100/is same tree by DFS

--- leet_BTREE_0100_is_same_tree.py
+++ leet_BTREE_0100_is_same_tree.py
@@ -4,11 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None:
+        if not p and not q:
             return True
-        if p is None or q is None or p.val != q.val:
+        if not p or not q or p.val != q.val:
             return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return self.isSameTree(p.left, q.left) and \
+            self.isSameTree(p.right, q.right)
