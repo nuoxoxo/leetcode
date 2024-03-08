5fa22039567f69d903e7785f1a0fe04e789c11ef
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-08 09:53:14 +0100

    3005/cc - map to vector sorted by DIY comparator

---
+++ leet_3005_max_freq.cc
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    using PII = pair<int, int>;
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> D;
+        for (int n: nums)
+            D[n]++;
+        vector<PII> vp;
+        for (auto & it: D)
+            vp.push_back(it);
+        sort(vp.begin(), vp.end(), cmp);
+        int top = vp[0].second;
+        int i = -1, res = 0;
+        while (++i < (int)vp.size())
+        {
+            if (vp[i].second == top)
+                res += top;
+            else
+                break;
+        }
+        return res;
+    }
+
+    static bool cmp(PII & l, PII & r)
+    {
+        return l.second > r.second;
+    }
+};
