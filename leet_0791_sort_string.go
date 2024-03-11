c3bc4a4cab7667b04c12a64d69a8682b369a3c25
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-11 09:06:50 +0100

    791/go : added custom sort, edit map

--- leet_0791_sort_string.go
+++ leet_0791_sort_string.go
@@ -1,25 +1,35 @@
 func customSortString(order string, s string) string {
     Solutions := []func(string, string)string{
+        // Custom_sort,
         Using_map,
     }
     return Solutions[0](order, s)
 }
 
+func Custom_sort(order string, s string) string {
+
+    S := []rune(s)
+    sort.SliceStable(S, func(l, r int)bool{
+		return strings.IndexRune(order, S[l]) < strings.IndexRune(order, S[r])
+	})
+    return string(S)
+}
+
 func Using_map(order string, s string) string {
+
     D := make(map[rune]int)
     for _, c := range s {
         D[c]++
     }
     res := []rune{}
     for _, c := range order {
-        n, ok := D[c]
-        if ok {
+        if n, ok := D[c]; ok {
             i := 0
             for i < n {
                 res = append(res, c)
                 i++
             }
-            D[c] = 0
+            delete(D, c)
         }
     }
     for c, n := range D {
