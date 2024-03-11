ab28e142993229fc9baf94659d2b28bcdb709a99
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-11 08:52:35 +0100

    791/py : using dict is faster

--- leet_0791_sort_string.py
+++ leet_0791_sort_string.py
@@ -1,5 +1,25 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        return [
+            self.Using_dict, # Faster
+            self.Using_set,
+        ][ 0 ]( order, s )
+
+    def Using_dict(self, order: str, s: str) -> str:
+        D = dict(Counter(s))
+        res = []
+        for c in order:
+            if c not in D:
+                continue
+            for _ in range(D[c]):
+                res.append(c)
+            D[c] = 0
+        for char, times in D.items():
+            for _ in range(times):
+                res.append(char)
+        return ''.join(res)
+
+    def Using_set(self, order: str, s: str) -> str:
         S, O = set(s), set(order)
         R = []
         for c in list(S - O):
