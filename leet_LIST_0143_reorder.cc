a94cf960472afc96110698921bdfd8570bae8617
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-23 17:59:32 +0100

    143/cc - added new soln - reverse partial list

--- leet_LIST_0143_reorder.cc
+++ leet_LIST_0143_reorder.cc
@@ -13,11 +13,46 @@ public:
     void reorderList(ListNode* head)
     {
         vector<void(*)(ListNode *)> Solutions {
-            Extra_space,
+            // Extra_space,
+            Reverse_linked_list,
         };
         Solutions[0](head);
     }
 
+    static void Reverse_linked_list (ListNode *head)
+    {
+        if (!head || !head->next)
+            return ;
+        ListNode * f = head, * s = head;
+        while (f && f->next)
+        {
+            f = f->next->next;
+            s = s->next;
+        }
+        ListNode * end, * nxt;
+        end = s;
+        nxt = s->next;
+        s->next = nullptr ;
+        s = nxt;
+        while (s)
+        {
+            nxt = s->next;
+            s->next = end;
+            end = s;
+            s = nxt;
+        }
+        s = head;
+        while (s && end && s != end && s->next != end)
+        {
+            nxt = s->next;
+            if (!nxt)
+                break ;
+            ListNode * new_node = new ListNode(end->val, nxt);
+            s->next = new_node;
+            s = nxt;
+            end = end->next;
+        }
+    }
 
     static void Extra_space (ListNode * head)
     {
