fb3b70eaac21dda59a8e3f046b0bbcde61334241
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-24 21:00:58 +0100

    287/re-edit cc

--- leet_BS_0287_find_dup_num.cc
+++ leet_BS_0287_find_dup_num.cc
@@ -2,22 +2,41 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums)
     {
-        int len = nums.size();
-        if (len == 2)
+        vector<int(*)(vector<int>&)> Solutions {
+            Binary_search,
+            // Naive,
+        };
+        return Solutions[0](nums);
+    }
+
+    static int Binary_search(vector<int>& nums)
+    {
+        int N = (int) nums.size();
+        if (N < 3)
             return nums[0];
+        int mid = N / 2;
         sort(nums.begin(), nums.end());
-        int i = 1;
-        int j = len - 2;
-        int mid = len / 2;
-        while (i ^ j && i < mid && j > mid)
+        if (nums[mid - 1] == nums[mid] || nums[mid] == nums[mid + 1])
+            return nums[mid];
+        int l = 1, r = N - 2;
+        while (l < mid && mid < r)
         {
-            if (nums[i] == nums[i - 1])
-                return nums[i];
-            if (nums[j] == nums[j + 1])
-                return nums[j];
-            ++i;
-            --j;
+            if (nums[l - 1] == nums[l]) return nums[l];
+            if (nums[r + 1] == nums[r]) return nums[r];
+            ++l;
+            --r;
         }
+        assert(false);
         return nums[mid];
+    }
+
+    static int Naive(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while (++i < (int)nums.size())
+            if (nums[i - 1] == nums[i])
+                break ;
+        return nums[i];
     }
 };
