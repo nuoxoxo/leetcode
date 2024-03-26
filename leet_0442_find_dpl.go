935331cf191bfe8855c6993488a284bd8fae7047
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-26 10:45:43 +0100

    442/go - added constant space Cyclic sort soln

---
+++ leet_0442_find_dpl.go
@@ -0,0 +1,45 @@
+func findDuplicates(nums []int) []int {
+
+    Solutions := []func([]int)[]int {
+        // Using_extra_space,
+        Cyclic_sort,
+    }
+    return Solutions[0](nums)
+}
+
+func Cyclic_sort(nums []int) []int {
+    res := []int{}
+    N := len(nums)
+    i := 0
+    for i < N {
+        for nums[i] != i + 1 && nums[i] != nums[nums[i] - 1] {
+            nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+        }
+        // Cyclic sort - idea :
+        //      swap the current number to its correct index
+        //      until it's in the right place
+        i++
+    }
+    i = 0
+    for i < N {
+        if nums[i] != i + 1 {
+            res = append(res, nums[i])
+        }
+        i++
+    }
+    return res
+}
+
+func Using_extra_space(nums []int) []int {
+    N := len(nums)
+    res := []int{}
+    D := make([]int, N)
+    for _, n := range nums {
+        if D[n - 1] == 1 {
+            res = append(res, n)
+        } else {
+            D[n - 1]++
+        }
+    }
+    return res
+}
