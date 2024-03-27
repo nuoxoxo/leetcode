9309a6beb298aaccea5f401d53fba477815dc764
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-03-27 13:43:52 +0100

    713/cc - edit

--- leet_SLWD_0713_subproduct.cc
+++ leet_SLWD_0713_subproduct.cc
@@ -2,25 +2,30 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k)
     {
+        vector<int(*)(vector<int> &, int)> Solutions {
+            Two_pointers,
+        };
+        return Solutions[0](nums, k);
+    }
+
+    static int Two_pointers(vector<int>& nums, int k)
+    {
         if (k < 2)
-            return 0 ;
-
-        int count, prod, i, j;
-
-        count = 0;
-        prod = 1;
-        i = -1; // fast ptr
-        j = 0;	// slow ptr
-        while (++i < nums.size())
+            return 0;
+        int res = 0;
+        int prod = 1;
+        int R = -1;
+        int L = 0;
+        while (++R < nums.size())
         {
-            prod *= nums[i];
+            prod *= nums[ R ];
             while (prod >= k)
             {
-                prod /= nums[j];
-                ++j;
+                prod /= nums[ L ];
+                ++L;
             }
-            count += i - j + 1;
+            res += R - L + 1;
         }
-        return count ;
+        return res;
     }
 };
