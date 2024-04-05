76345115e9c95208d03a33f20556caa270cbec20
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-05 08:50:05 +0200

    1544/py - scan & remove - 2nd soln

--- leet_1544_great_str.py
+++ leet_1544_great_str.py
@@ -1,5 +1,20 @@
 class Solution:
     def makeGood(self, s: str) -> str:
+        return [
+            # self.Using_stack_like,
+            self.Linear_scan_and_remove,
+        ][0](s)
+
+    def Linear_scan_and_remove(self, s: str) -> str:
+        dq = list(s)
+        for i in range(len(dq) - 1):
+            l, r = dq[i], dq[i + 1]
+            if l != r and l.lower() == r.lower():
+                dq = dq[:i] + dq[i + 2:]
+                i -= 1
+        return ''.join(dq)
+
+    def Using_Stack_like(self, s: str) -> str:
         dq = []
         for c in s:
             dq.append(c)
