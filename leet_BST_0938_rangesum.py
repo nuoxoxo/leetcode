b511e04e311a807ffc9d5b106b0f062a4676f48d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-17 01:38:07 +0200

    938/py - combine DFS & BFS

---
+++ leet_BST_0938_rangesum.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        return [
+            # self.BFS,
+            self.DFS
+        ][ 0 ]( root, low, high )
+
+    def BFS(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        Q = deque()
+        Q.append(root)
+        res = []
+        while Q:
+            node = Q.popleft()
+            if low <= node.val <= high:
+                res.append(node.val)
+            if node.left:
+                Q.append(node.left)
+            if node.right:
+                Q.append(node.right)
+        return sum(res)
+
+    def DFS(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        res = 0
+        def recurse(node):
+            if not node:
+                return
+            nonlocal res
+            if low <= node.val <= high:
+                res += node.val
+            if low < node.val:
+                recurse(node.left)
+            if node.val < high:
+                recurse(node.right)
+        recurse(root)
+        return res
