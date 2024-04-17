cadb88ac84296ea1f39edd0789e58de3a1c7020c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-17 02:12:03 +0200

    938/c - Inorder is fast

--- leet_BST_0938_rangesum.c
+++ leet_BST_0938_rangesum.c
@@ -11,8 +11,20 @@ void DFS(struct TreeNode *, int, int, int*);
 int rangeSumBST(struct TreeNode* root, int low, int high)
 {
     int res = 0;
-    DFS(root, low, high, & res);
+    // DFS(root, low, high, & res);
+    Inorder(root, low, high, & res);
     return res;
+}
+
+void Inorder(struct TreeNode* node, int low, int high, int * res)
+{
+    if ( ! node)
+        return ;
+    Inorder(node->left, low, high, res);
+    int n = node->val;
+    if (low <= n && n <= high)
+        * res += n;
+    Inorder(node->right, low, high, res);
 }
 
 void DFS(struct TreeNode* node, int low, int high, int * res)
