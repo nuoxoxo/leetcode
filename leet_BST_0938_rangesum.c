188282b47e65e3a23748a41cefcd46bece5d7214
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-17 02:22:25 +0200

    938/c - combine DFS & BFS + inorder

--- leet_BST_0938_rangesum.c
+++ leet_BST_0938_rangesum.c
@@ -1,48 +1,68 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-void DFS(struct TreeNode *, int, int, int*);
-
-int rangeSumBST(struct TreeNode* root, int low, int high)
-{
-    int res = 0;
-    // DFS(root, low, high, & res);
-    Inorder(root, low, high, & res);
-    return res;
-}
-
-void Inorder(struct TreeNode* node, int low, int high, int * res)
-{
-    if ( ! node)
-        return ;
-    Inorder(node->left, low, high, res);
-    int n = node->val;
-    if (low <= n && n <= high)
-        * res += n;
-    Inorder(node->right, low, high, res);
-}
-
-void DFS(struct TreeNode* node, int low, int high, int * res)
-{
-    if ( ! node)
-        return ;
-    int n = node->val;
-    if (low <= n && n <= high)
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high)
     {
-        * res += n;
+        vector<int(*)(TreeNode *, int, int)> Solutions {
+            // BFS,
+            // DFS,
+            inorder,
+        };
+        return Solutions[0](root, low, high);
     }
-    if (low < n)
-    {
-        DFS(node->left, low, high, res);
-    }
-    if (n < high)
-    {
-        DFS(node->right, low, high, res);
-    }
-}
 
+    static int inorder(TreeNode* root, int low, int high)
+    {
+        int res = 0;
+        std::function<void(TreeNode *)> traverse = [&](TreeNode * node)
+        {
+            if (!node)
+                return ;
+            traverse(node->left);
+            int n = node->val;
+            if (low <= n && n <= high)
+                res += n;
+            traverse(node->right);
+        };
+        traverse(root);
+        return res;
+    }
+
+    static int BFS(TreeNode* root, int low, int high)
+    {
+        assert( root );
+        deque<TreeNode *> D{root};
+        vector<int> vi;
+        while ( ! D.empty())
+        {
+            TreeNode * node = D.front();
+            D.pop_front();
+            int n = node->val;
+            if (low <= n && n <= high)
+                vi.push_back(n);
+            if (node->left)
+                D.push_back(node->left);
+            if (node->right)
+                D.push_back(node->right);
+        }
+        return std::accumulate(vi.begin(), vi.end(), 0);
+    }
+
+    static int DFS(TreeNode * root, int low, int high)
+    {
+        int res = 0;
+        std::function< void(TreeNode *) > recurse = [&](TreeNode * node)
+        {
+            if ( ! node)
+                return ;
+            int n = node->val;
+            if (low <= n && n <= high)
+                res += n;
+            if (low < n)
+                recurse(node->left);
+            if (n < high)
+                recurse(node->right);
+        };
+        recurse( root );
+        return res;
+    }
+};
