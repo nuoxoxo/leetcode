e8e260f2f148c3c4a08414f15611382aad5825ed
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-18 12:16:10 +0200

    463/added c - using math - fixed

--- leet_0463_perimter.c
+++ leet_0463_perimter.c
@@ -11,9 +11,9 @@ int islandPerimeter(int** grid, int gridSize, int* gridColSize) {
             if (grid[r][c] == 1)
             {
                 res += 4;
-                if (r > 0 && grid[r][c] == 1)
+                if (r > 0 && grid[r - 1][c] == 1)
                     res -= 2;
-                if (c > 0 && grid[r][c] == 1)
+                if (c > 0 && grid[r][c - 1] == 1)
                     res -= 2;
             }
         }
