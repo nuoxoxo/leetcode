e7829f04f7562ffc40abbf91acb272f30c4c51d3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-19 11:52:32 +0200

    200/py - floodfill - dfs and bfs

--- leet_BFS_DFS_0200_count_islands.py
+++ leet_BFS_DFS_0200_count_islands.py
@@ -2,7 +2,34 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         return [
             self.Floodfill,
+            self.BFS,
         ][0](grid)
+
+    def BFS(self, G: List[List[str]]) -> int:
+        R, C = len(G), len(G[0])
+        dr = [-1, 0, 1, 0]
+        dc = [0, 1, 0, -1]
+        seen = set()
+        Q = deque()
+        res = 0
+        for r in range(R):
+            for c in range(C):
+                if G[r][c] == '1' and (r, c) not in seen:
+                    res += 1
+                    seen.add( (r, c) )
+                    Q.append( (r, c) )
+
+                    # Floodfill using BFS
+                    while Q:
+                        node = Q.popleft()
+                        for i in range(4):
+                            rr = node[0] + dr[i]
+                            cc = node[1] + dc[i]
+                            if -1 < rr < R and -1 < cc < C and \
+                                G[rr][cc] == '1' and (rr, cc) not in seen:
+                                Q.append( (rr, cc) )
+                                seen.add( (rr, cc) )
+        return res
 
     def Floodfill(self, G: List[List[str]]) -> int:
         R, C = len(G), len(G[0])
