5544a99097c7ab84829ec6804012d2bbb63f3b51
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-20 22:16:00 +0200

    1992/py - del sr, sc

--- leet_1992_farmland.py
+++ leet_1992_farmland.py
@@ -11,15 +11,15 @@ class Solution:
         for r in range(R):
             for c in range(C):
                 if land[r][c] == 1 and (r, c) not in Seen:
-                    sr, sc, er, ec = r, c, r, c
+                    er, ec = r, c
                     # no need to check set bc. it says 
                     #   "no two groups are adjacent"
                     while er + 1 < R and land[er + 1][ec] == 1:# and (er + 1, ec) not in Seen:
                         er += 1
                     while ec + 1 < C and land[er][ec + 1] == 1:# and (er, ec + 1) not in Seen:
                         ec += 1
-                    res.append([sr, sc, er, ec])
-                    for i in range(sr, er + 1):
-                        for j in range(sc, ec + 1):
+                    res.append([r, c, er, ec])
+                    for i in range(r, er + 1):
+                        for j in range(c, ec + 1):
                             Seen.add((i, j))
         return res
