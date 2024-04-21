950e2c8610e0e80e4b339635fb9cdea56df1784a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-21 11:20:11 +0200

    1971/cc - updated to proper BFS

--- leet_BFS_1971_path_exists.cc
+++ leet_BFS_1971_path_exists.cc
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int src, int des)
-    {
-        vector<int> total[n];
-        deque<int>  dq;
-        bool        v[n];
-        int         i, m;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
 
-        memset(v, false, sizeof(v));
-        m = (int) edges.size();
-        i = -1;
-        while (++i < m)
-        {
-            total[edges[i][0]].push_back(edges[i][1]);
-            total[edges[i][1]].push_back(edges[i][0]);
+        vector<bool(*)(int, vector<vector<int>>&, int, int)> Solutions {
+            BFS,
+        };
+        return Solutions[0](n, edges, source, destination);
+    }
+
+    static bool BFS (int n, vector<vector<int>>& edges, int source, int destination)
+    {
+        (void) n;
+        set<int> Seen;
+        Seen.insert(source);
+        deque<int> Q;
+        Q.push_back(source);
+        unordered_map<int, vector<int>> ADJ;
+        // prepare adjacency list
+        for (auto & edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            ADJ[u].push_back(v);
+            ADJ[v].push_back(u);
         }
-        dq.push_back(src);
-        while(!dq.empty())
-        {
-            int p;
-            
-            p = dq.front();
-            dq.pop_front();
-            if (p == des)
+        while (!Q.empty()) {
+            int u = Q.front();
+            if (u == destination)
                 return true;
-            i = -1;
-            while (++i < (int) total[p].size())
-            {
-                if (!v[total[p][i]])
-                {
-                    dq.push_back(total[p][i]);
-                    v[total[p][i]] = true;
+            Q.pop_front();
+            for (int v : ADJ[u]) {
+                if (Seen.find(v) == Seen.end()) {
+                    Seen.insert(v);
+                    Q.push_back(v);
                 }
             }
         }
