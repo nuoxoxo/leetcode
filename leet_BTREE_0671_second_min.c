81dbf51fa0911666aa868195f6d7a74af6022d7a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-22 23:20:08 +0200

    671/c - DFS - + alt. vers

--- leet_BTREE_0671_second_min.c
+++ leet_BTREE_0671_second_min.c
@@ -10,17 +10,41 @@
 long long res0 = 2147483648;
 long long res1 = 2147483648;
 
-void recurse(struct TreeNode *);
+long long res = 2147483648;
+
+void DFS_1(struct TreeNode *);
+void DFS_2(struct TreeNode *, int *);
 
 int findSecondMinimumValue(struct TreeNode* root) {
 
+    // vers. 1
     res0 = 2147483648;
     res1 = 2147483648;
-    recurse(root);
-    return res1 ^ 2147483648 ? (int) res1 : -1;
+    DFS_1(root);
+    // return res1 ^ 2147483648 ? (int) res1 : -1;
+
+    /* ****** ****** ****** */
+
+    // vers. 2
+    res = 2147483648;
+    DFS_2(root, &(root->val));
+    return res ^ 2147483648 ? (int) res : -1;
 }
 
-void recurse(struct TreeNode * node) {
+void DFS_2(struct TreeNode * node, int * curr) {
+
+    if (!node)
+        return ;
+    int n = node->val;
+    if ( n != *curr && n < res)
+        res = n;
+    if (res <= n)
+        return ;
+    DFS_2(node->left, curr);
+    DFS_2(node->right, curr);
+}
+
+void DFS_1(struct TreeNode * node) {
 
     if (!node)
         return ;
@@ -34,7 +58,7 @@ void recurse(struct TreeNode * node) {
     {
         res1 = n;
     }
-    recurse(node->left);
-    recurse(node->right);
+    DFS_1(node->left);
+    DFS_1(node->right);
 }
 
