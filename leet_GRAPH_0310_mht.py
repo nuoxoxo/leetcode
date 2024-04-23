8ac344e9f36afed555ef26557b98a93869bd7c98
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-23 10:48:32 +0200

    310/min height trees - BFS until only centroids left

---
+++ leet_GRAPH_0310_mht.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+
+        # Basic idea
+        #   - finds the leaves of the tree iteratively,
+        #   - until only the centroids remain ie. roots of the MHTs
+
+        res = [] # leaves / initial leaves
+        ADJ = defaultdict(list)
+        for u, v in edges:
+            ADJ[u].append(v)
+            ADJ[v].append(u)
+        degrees = [None] * n
+        for i in range(n):
+            degrees[i] = len(ADJ[i])
+
+            # initial leaves
+            #   - nodes with only one edge
+            if degrees[i] == 1:
+                res.append(i)
+                degrees[i] -= 1
+        print('res/', res)
+        print('deg/', degrees)
+        while True:
+            # remove leaves until only the centroids ie. roots of MHTs remain
+            #   - it is Greedy
+            #   - reduce the tree's dimension in each iteration by removing leaves
+            #   - once there are no new leaves, all there remains are centroids
+            new_leaves = []
+            for u in res:
+                for v in ADJ[u]:
+                    if degrees[v] > 0:
+                        degrees[v] -= 1
+                        if degrees[v] == 1:
+                            new_leaves.append(v)
+            if not new_leaves:
+                break
+            print('res/', new_leaves)
+            res = new_leaves
+        return res
+
