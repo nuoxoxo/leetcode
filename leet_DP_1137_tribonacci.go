ed84e6dafefc5c43c086ce39f742808df709047c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-24 13:23:40 +0200

    1137/Go - recursion is TLE

--- leet_DP_1137_tribonacci.go
+++ leet_DP_1137_tribonacci.go
@@ -1,15 +1,54 @@
 func tribonacci(n int) int {
+
+    Solutions := []func(int)int{
+        // Tabulation1,
+        // Tabulation2,
+        Recurse, 
+    }
+    return Solutions[0](n)
+}
+
+func Recurse(n int) int {
+
     if n < 2 {
         return n
     }
-    f := make([]int, n + 1)
-    f[0] = 0
-    f[1] = 1
-    f[2] = 1
+    if n == 2 {
+        return 1
+    }
+    return Recurse(n - 3) + Recurse(n - 2) + Recurse(n - 1)
+}
+
+func Tabulation2(n int) int {
+
+    tr := []int{0, 1, 1}
+    if n < 3 {
+        return tr[n]
+    }
     i := 3
     for i < n + 1 {
-        f[i] = f[i - 1] + f[i - 2] + f[i - 3]
-        i++ 
+        tr = append(tr, tr[i - 3] + tr[i - 2] + tr[i - 1])
+        i++
     }
-    return f[n]
+    return tr[n]
+}
+
+func Tabulation1(n int) int {
+
+    tr := make([]int, n + 1)
+    if n == 0 {
+        return n
+    }
+    if n < 3 {
+        return 1
+    }
+    tr[0] = 0
+    tr[1] = 1
+    tr[2] = 1
+    i := 3
+    for i < n + 1 {
+        tr[i] = tr[i - 3] + tr[i - 2] + tr[i - 1]
+        i++
+    }
+    return tr[n]
 }
