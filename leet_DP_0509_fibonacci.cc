55303dab6258f0dd677e7100e3cc6de79375fbd5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-24 13:33:31 +0200

    509/cc - upd.

--- leet_DP_0509_fibonacci.cc
+++ leet_DP_0509_fibonacci.cc
@@ -1,17 +1,45 @@
 class Solution {
 public:
-    int fib(int n)
-    {
-        int     f[n + 1];
-        int     i;
+    int fib(int n) {
+        vector<int(*)(int)> Solutions {
+            Tabulation_int_array,
+            Tabulation_vector,
+            // Recurse, // TLE
+        };
+        return Solutions[0](n);
+    }
+
+
+    static int Recurse(int n) {
 
         if (n < 2)
-            return (n);
-        f[0] = 0;
-        f[1] = 1;
-        i = 1;
-        while (++i < n + 1)
-            f[i] = f[i - 2] + f[i - 1];
-        return (f[n]);
+            return n;
+        return Recurse(n - 1) + Recurse(n - 2);
+    }
+
+
+    static int Tabulation_int_array(int n) {
+
+        if (n < 2)
+            return n;
+        int tr[n + 1];
+        tr[0] = 0;
+        tr[1] = 1;
+        int i = 1;
+        while (++i < n + 1) {
+            tr[i] = tr[i - 2] + tr[i - 1];
+        }
+        return tr[n];
+    }
+
+
+    static int Tabulation_vector(int n) {
+
+        vector<int> tr {0, 1};
+        int i = 1;
+        while (++i < n + 1) {
+            tr.push_back(tr[i - 2] + tr[i - 1]);
+        }
+        return tr[n];
     }
 };
