61b4cd1487d73e7a8332eda7fc649c4d4e6f58e9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-25 21:15:05 +0200

    300/3 soln : nSq dp, BFs, BisectLeft

--- leet_DPBS_0300_LIS.py
+++ leet_DPBS_0300_LIS.py
@@ -1,19 +1,56 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        temp = []
+        return [
+            # self.nSquare,
+            # self.using_bisect_left,   # faster than Nsquare
+            self.binary_search,         # fastest
+        ][0](nums)
+
+    def binary_search(self, nums: List[int]) -> int:
+        lens = []
         for n in nums:
-            size = len(temp)
-            if size == 0 or n > temp[-1]:
-                temp.append(n)
-                # print(temp)
+            N = len(lens)
+            if not lens or lens[-1] < n:
+                lens.append(n)
             else:
                 L = 0
-                R = size - 1
+                R = N - 1
                 while L < R:
                     mid = (R - L) // 2 + L
-                    if temp[mid] < n:
+                    if lens[mid] < n:
                         L = mid + 1
                     else:
                         R = mid
-                temp[R] = n
-        return len(temp)
+                lens[R] = n
+        return len(lens)
+
+    def using_bisect_left(self, nums: List[int]) -> int:
+        lens = []
+        for n in nums:
+            i = bisect.bisect_left(lens, n)
+            if i == len(lens):
+                lens.append(n)
+            else:
+                lens[i] = n
+        return len(lens)
+
+    def nSquare(self, nums: List[int]) -> int:
+        N = len(nums)
+        INF = 10 ** 20
+        lens = [INF] * (N + 1)
+        lens[0] = -INF
+        for n in nums:
+            for i in range(0, N + 1):
+                if lens[i] < n:
+                    if lens[i + 1] > n:
+                        lens[i + 1] = n
+            # print(n)
+            # print(lens)
+        res = -1
+        for i in range(N, -1, -1):
+            if lens[i] != INF:
+                res = i
+                break
+        # print(lens)
+        return res
+
