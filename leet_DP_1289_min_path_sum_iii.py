e9895913053306ee97f1d2f9a9ea80fc070ba1aa
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-26 11:31:06 +0200

    1289/hard pb: O(n^3) DP - now added Fast DP min/2nd min tracking

--- leet_DP_1289_min_path_sum_iii.py
+++ leet_DP_1289_min_path_sum_iii.py
@@ -9,11 +9,11 @@ class Solution:
         35 - 357,359
         """
         return [
-            self.nCubed,
+            self.min_and_2nd_min_tracker,
+            # self.nCubed,
         ][0](grid)
 
-    """
-    def faster_dp(self, grid: List[List[int]]) -> int:
+    def min_and_2nd_min_tracker(self, grid: List[List[int]]) -> int:
 
         INF = 10 ** 20
         N = len(grid)
@@ -22,7 +22,27 @@ class Solution:
         for c in range(C):
             dp[0][c] = grid[0][c]
         for r in range(1, R):
-    """
+            # keep track of min and 2nd min values in the PREV row
+            min_val, second_min_val = INF, INF
+            min_idx, second_min_idx = -1, -1
+            for c in range(C):
+                if dp[r - 1][c] < min_val:
+                    # branched out (1) - upd. min val if a smaller one is found
+                    second_min_idx = min_idx
+                    second_min_val = min_val
+                    min_val = dp[r - 1][c]
+                    min_idx = c
+                elif dp[r - 1][c] < second_min_val:
+                    # branched out (2) - when `min_val < PreviousRow[c] < second_min_val`
+                    second_min_idx = c
+                    second_min_val = dp[r - 1][c]
+            # now upd. the current row while minding the adj. index
+            for c in range(C):
+                if c != min_idx:
+                    dp[r][c] = grid[r][c] + min_val
+                else:
+                    dp[r][c] = grid[r][c] + second_min_val
+        return min(dp[-1])
 
 
     def nCubed(self, grid: List[List[int]]) -> int:
@@ -42,4 +62,4 @@ class Solution:
                     temp = dp[r - 1][idx] + grid[r][c]
                     dp[r][c] = min(dp[r][c], temp)
         return min(dp[-1])
-        
+
