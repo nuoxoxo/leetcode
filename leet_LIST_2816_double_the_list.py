b629d1eb1143c85c1f5dfa546b29583b63bc29bd
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-07 09:14:50 +0200

    2816/bruteforce, 3pass - calc, make list, reverse

---
+++ leet_LIST_2816_double_the_list.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        num = 0
+        node = head
+        while node:
+            num = num * 10 + node.val
+            node = node.next
+        num *= 2
+        if num == 0:
+            return ListNode(0)
+        # make list
+        node = ListNode()
+        _head = node
+        while num > 0:
+            node.val = num % 10
+            node.next = ListNode()
+            node = node.next
+            num //= 10
+        # reverse
+        _prev, _next = None, None
+        node = _head
+        while node:
+            _next = node.next
+            node.next = _prev
+            _prev = node
+            _head = _prev # track head
+            node = _next
+        return _head.next
+
+
