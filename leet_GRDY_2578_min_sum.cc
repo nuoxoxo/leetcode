014110a93e8d742d2adcf376c0e50ab6758aa450
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-13 09:33:15 +0200

    2578/just sort it - edit cc

--- leet_GRDY_2578_min_sum.cc
+++ leet_GRDY_2578_min_sum.cc
@@ -1,6 +1,38 @@
 class Solution {
 public:
     int splitNum(int num) {
+        vector<int(*)(int)> solutions {
+            // sorted_string,
+            sorted_ints,
+        };
+        return solutions[0](num);
+    }
+
+    static int sorted_ints(int num)
+    {
+        vector<int> arr;
+        while (num)
+        {
+            int n = num % 10;
+            arr.push_back(n);
+            num /= 10;
+        }
+        sort(begin(arr), end(arr));
+        int L = 0, R = 0;
+        int i = -1;
+        while (++i < arr.size())
+        {
+            int n = arr[i];
+            if (i % 2)
+                L = L * 10 + n;
+            else 
+                R = R * 10 + n;
+        }
+        return L + R;
+    }
+
+    static int sorted_string(int num)
+    {
         string s = std::to_string(num);
         sort(begin(s), end(s));
         int L = 0, R = 0;
