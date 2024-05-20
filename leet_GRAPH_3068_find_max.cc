12a10f37695a9100bbaafbe702aa6b03e88eb7e7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-20 21:28:11 +0200

    3048/a hard one - greedy

---
+++ leet_GRAPH_3068_find_max.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges)
+    {
+        long long tt = 0;
+        for (int n: nums)
+            tt += n;
+
+        vector<long long > updates;
+        for (int n: nums)
+            updates.push_back((n ^ k) - n);
+        sort(begin(updates), end(updates), std::greater<int>()); 
+       
+        long long  res = tt;
+        int N = nums.size();
+        int i = 0;
+        while (i + 1 < N)
+        {
+            tt += updates[i] + updates[i + 1];
+            res = max(res, tt);
+            i += 2;
+        }
+        return res;
+    }
+};
