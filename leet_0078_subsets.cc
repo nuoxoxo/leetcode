daec54166ce43391e87b0e37a0fe6ee28b23aabb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-21 08:12:49 +0200

    78/iteratively added every valid sub-set - new soln

--- leet_0078_subsets.cc
+++ leet_0078_subsets.cc
@@ -2,43 +2,18 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums)
     {
-        int                 s, subs, i, j;
-        vector<vector<int>> E;
-
-        E.push_back({});
-        s = nums.size();
-        i = -1;
-        while (++i < s)
+        int i = -1;
+        vector<vector<int>> res = {{}};
+        for (int n: nums)
         {
-            subs = E.size();
-            j = -1;
-            while (++j < subs)
+            int S = res.size();
+            int j = -1;
+            while (++j < S)
             {
-                // cout << i << ".1: ";
-                E.push_back(E[j]);
-                // pa(E);
-                E.back().push_back(nums[i]);
-                // cout << i << ".2: ";
-                // pa(E);
+                res.push_back(res[j]);
+                res.back().push_back(n);
             }
         }
-        return E ;
+        return res;
     }
-    
-    // void pa(vector<vector<int>> E)
-    // {
-    //     int i, j;
-    //     i = -1;
-    //     while (++i < E.size())
-    //     {
-    //         j = -1;
-    //         cout << "[ ";
-    //         while (++j < E[i].size())
-    //         {
-    //             cout << E[i][j] << ' ';
-    //         }
-    //         cout << "]";
-    //     }
-    //     cout << endl;
-    // }
 };
