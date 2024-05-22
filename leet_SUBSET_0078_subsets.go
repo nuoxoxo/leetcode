da987a547e07702df92a12df92d8641681d6dd6a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-22 22:58:04 +0200

    78/ 2 solns - backtracking & iterative bruteforce - GO

--- leet_SUBSET_0078_subsets.go
+++ leet_SUBSET_0078_subsets.go
@@ -1,5 +1,35 @@
 func subsets(nums []int) [][]int {
 
+    solutions := []func([]int)[][]int {
+        // iterative_bruteforce,
+        backtracking,
+    }
+    return solutions[0](nums)
+}
+
+func backtracking(nums []int) [][]int {
+
+    N := len(nums)
+    res := [][]int{}
+
+    var Backtrack func(int, []int)
+    Backtrack = func(i int, curr []int) {
+        if i == N {
+            res = append(res, append([]int{}, curr...))
+            return
+        }
+        Backtrack(i + 1, curr)
+        curr = append(curr, nums[i])
+        Backtrack(i + 1, curr)
+        curr = curr[:len(curr) - 1]
+    }
+
+    Backtrack(0, []int{})
+    return res
+}
+
+func iterative_bruteforce(nums []int) [][]int {
+
     res := [][]int{ []int{} }
 
     for _, n := range nums {
