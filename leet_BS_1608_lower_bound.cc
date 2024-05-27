f49b89fbc51c27bebf84de71a487220262ffa09e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-05-27 16:16:45 +0200

    1608/two solns - std and custom lower bound

--- leet_BS_1608_lower_bound.cc
+++ leet_BS_1608_lower_bound.cc
@@ -2,15 +2,52 @@ class Solution {
 public:
     int specialArray(vector<int>& nums)
     {
-        sort(nums.begin(), nums.end());
-        int len = (int) nums.size();
-        int k = -1;
-        while (++k < len + 1)
+        vector<int(*)(vector<int>&)> Solutions {
+            // Bruteforce_custom_lower_bound,
+            Bruteforce_std_lower_bound,
+        };
+        return Solutions[0](nums);
+    }
+
+    static int Bruteforce_std_lower_bound(vector<int>& nums)
+    {
+        sort(begin(nums), end(nums));
+        int N = nums.size();
+        int curr = -1;
+        while (++curr < N + 1)
         {
-            int diff = nums.end() - std::lower_bound(nums.begin(), nums.end(), k);
-            if (diff == k)
-                return k;
+            auto L = std::lower_bound(begin(nums), end(nums), curr);
+            if (curr == end(nums) - L)
+                return curr;
         }
-        return -1; 
+        return -1;
+    }
+
+    static int Bruteforce_custom_lower_bound(vector<int>& nums)
+    {
+        sort(begin(nums), end(nums));
+        int N = nums.size();
+        int curr = -1;
+        while (++curr < N + 1)
+        {
+            int l = 0, r = N;
+            //  find bisect_left / lower_bound
+            while (l < r)
+            {
+                int mid = (r - l) / 2 + l;
+                {
+                    int n = nums[mid];
+                    if (n < curr)
+                        l = mid + 1;
+                    else
+                        r = mid;
+                }
+            }
+            if (curr == N - l)
+            {
+                return curr;
+            }
+        }
+        return -1;
     }
 };
