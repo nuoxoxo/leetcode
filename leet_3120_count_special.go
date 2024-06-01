3aa002625ff81cfbc903becccd36b6e64ff738ee
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-01 12:15:59 +0200

    3120/go - 2 pass

---
+++ leet_3120_count_special.go
@@ -0,0 +1,27 @@
+func numberOfSpecialChars(word string) int {
+
+    U, L := make([]bool, 26), make([]bool, 26)
+    i := 0
+    for i < 26 {
+        U[i] = false
+        L[i] = false
+        i++
+    }
+    for _, c := range word {
+        fg := unicode.IsLower(c)
+        if fg {
+            L[int(c) - 'a'] = true
+        } else {
+            U[int(c) - 'A'] = true
+        }
+    }
+    res := 0
+    i = 0
+    for i < 26 {
+        if U[i] && L[i] {
+            res++
+        }
+        i++
+    }
+    return res
+}
