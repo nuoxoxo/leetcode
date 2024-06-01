5a75c2b5fd30d90cd563fad03d8f435d8e8fb685
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-01 10:25:37 +0200

    3151/easy pb just figure out what parity means . fixed

--- leet_3151_parity.cc
+++ leet_3151_parity.cc
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums)
     {
-        int N = nums.size();
         int i = -1;
-        while (++i < N - 1)
+        while (++i < nums.size() - 1)
         {
             if ((nums[i] & 1) == (nums[i + 1] & 1))
                 return false;
