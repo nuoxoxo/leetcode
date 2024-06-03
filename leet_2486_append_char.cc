efd7fbb41c3963aa418e92a6d05cd3a247533b0a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-03 09:15:48 +0200

    2486/cc - added recursive soln

--- leet_2486_append_char.cc
+++ leet_2486_append_char.cc
@@ -2,6 +2,15 @@ class Solution {
 public:
     int appendCharacters(string s, string t)
     {
+        vector<int(*)(string, string)> solutions {
+            // Iterative,
+            Recursive,
+        };
+        return solutions[0](s, t);
+    }
+
+    static int Iterative(string s, string t)
+    {
         int lens = s.length(), lent = t.length();
         int si = -1, ti = 0;
         while (++si < lens)
@@ -14,5 +23,20 @@ public:
             }            
         }
         return lent - ti;
+    }
+
+    static int Recursive(string s, string t)
+    {
+        int lens = s.length(), lent = t.length();
+        std::function<int(int, int)> recurse = [&](int si, int ti) {
+            if (ti == lent)
+                return 0;
+            if (si == lens)
+                return lent - ti;
+            if (s[si] == t[ti])
+                return recurse(si + 1, ti + 1);
+            return recurse(si + 1, ti);
+        };
+        return recurse(0, 0);
     }
 };
