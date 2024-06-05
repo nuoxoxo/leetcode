3c760a56d13b46bb1b69f1e833191ca35b4e63fb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-05 16:21:05 +0200

    2215/ed.

--- leet_2215_array_diff___set.go
+++ leet_2215_array_diff___set.go
@@ -1,20 +1,20 @@
 func findDifference(nums1 []int, nums2 []int) [][]int {
-    s1 := map[int] bool {}
+    s1 := map[int]bool{}
     for _, n := range nums1 {
         if !s1[n] {
             s1[n] = true
         }
     }
 
-    s2 := map[int] bool {}
+    s2 := map[int]bool{}
     for _, n := range nums2 {
         if !s2[n] {
             s2[n] = true
         }
     }
 
-    a1 := [] int {}
-    a2 := [] int {}
+    a1 := []int{}
+    a2 := []int{}
     for n, _ := range s1 {
         if !s2[n] {
             a1 = append(a1, n)
@@ -25,5 +25,5 @@ func findDifference(nums1 []int, nums2 []int) [][]int {
             a2 = append(a2, n)
         }
     }
-    return [][] int {a1, a2}
+    return [][]int{a1, a2}
 }
