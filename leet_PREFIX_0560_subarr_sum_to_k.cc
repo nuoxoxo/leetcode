26ff7b127bed6264bb6dd232da90af5eb852ba6a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-09 23:16:05 +0200

    560/prefix sum + math

--- leet_PREFIX_0560_subarr_sum_to_k.cc
+++ leet_PREFIX_0560_subarr_sum_to_k.cc
@@ -1,20 +1,32 @@
 class Solution {
 public:
+
+    /*
+
+    a subarr can be expressed this way
+    -> a0 + a1 + a2 + ... + a[i - 1] + ai + ... + aj
+    ->                                 ^^^^^^^^^^^^^ = f(j) - f(i - 1)
+
+    SO a subarr sums to k IF :
+    -> f(j) - f(i - 1) === k, ... from which we can have
+    -> f(j) - k === f(i - 1) ... this is what we want
+    -> so, keeping track of DICT[f(j) - k] == counting how many such subarrays
+
+    */
+
     int subarraySum(vector<int>& nums, int k)
     {
-        map<int, int>   D ;
-        D[0] = 1;
-        int total = 0, count = 0;
-        for (int n: nums)
+        int res = 0;
+        int running = 0;
+        map<int, int> Prefix;
+
+        Prefix[0] = 1;
+        for (int n : nums)
         {
-            total += n;
-            if (D[total - k])
-                count += D[total - k];
-            if (D[total])
-                ++ D[total] ;
-            else
-                D[total] = 1;
+            running += n;
+            res += Prefix[running - k];
+            Prefix[running] += 1;
         }
-        return count ;
+        return res;
     }
 };
