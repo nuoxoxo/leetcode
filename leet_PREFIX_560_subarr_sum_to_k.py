dcf7f40ae2b97dab5fb8b4a8513508e5f8a531e6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-09 11:39:09 +0200

    560/ sim. to 974 - revision: prefix sum & math

--- leet_PREFIX_560_subarr_sum_to_k.py
+++ leet_PREFIX_560_subarr_sum_to_k.py
@@ -1,16 +1,30 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        d = {}
-        d[0] = 1
-        total = 0
-        count = 0
+
+        """
+        a subarr can be expressed this way
+        -> a0 + a1 + a2 + ... + a[i - 1] + ai + ... + aj
+        ->                                 ^^^^^^^^^^^^^ = f(j) - f(i - 1)
+        BECAUSE, in terms of line segment
+        -> ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ = f(j)
+        -> ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ = f(i - 1)
+
+        SO a subarr sums to k IF :
+        -> f(j) - f(i - 1) === k, ... from which we can have
+        -> f(j) - k === f(i - 1) ... this is what we want
+        -> so, keeping track of DICT[f(j) - k] == counting how many such subarrays
+        """
+
+        res = 0
+        prefix = defaultdict(int)
+        prefix[0] = 1
+        running = 0 # running prefix sum
         for n in nums:
-            total += n
-            if total - k in d:
-                count += d[total - k]
-            if total in d:
-                d[total] += 1
-            else:
-                d[total] = 1
-        return count
-        
+            running += n
+            j = running - k
+            res += prefix[j]
+            prefix[running] += 1
+        return res
+
+            
+
