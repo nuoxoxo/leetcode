0e80ff39220e7d805bc9b0a71c5914ab17489b0f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-10 08:55:06 +0200

    1051/rm old solns

--- leet_1051_height_check.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        h = sorted(heights)
-        res = 0
-        for i in range(len(h)):
-            if h[i] != heights[i]:
-                res += 1
-        return res
