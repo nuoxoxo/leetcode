e200aeb68d64ffa7c8ee86c6065a589f9300b7e3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-11 09:43:46 +0200

    1122/now has 2 ways - freq dict or index dict - py

--- leet_1122_relative_sort.py
+++ leet_1122_relative_sort.py
@@ -1,17 +1,32 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        # D = defaultdict()
-        D = {}
-        tail = []
-        res = []
-        for n in arr1:
-            if n not in arr2:
-                tail.append(n)
-        tail.sort()
-        for n in arr2:
-            D[n] = arr1.count(n)
-        # print(D)
-        for l, r in D.items():
-            res += [l] * r
-            print(res)
-        return res + tail
+        return [
+            # Frequency_map,
+            Index_map,
+        ][0](arr1, arr2)
+
+def Index_map(arr1: List[int], arr2: List[int]) -> List[int]:
+    D = defaultdict(int)
+    for i, n in enumerate(arr2):
+        D[n] = i
+    def cmpfunc(l, r) -> int:
+        if l in D and r in D:
+            return -1 if D[l] < D[r] else 1
+        elif l in D: return -1
+        elif r in D: return 1
+        return -1 if l < r else 1 if l > r else 0
+    return sorted(arr1, key=cmp_to_key(cmpfunc))
+
+def Frequency_map(arr1: List[int], arr2: List[int]) -> List[int]:
+    D = defaultdict(int)
+    tail = []
+    res = []
+    for n in arr1:
+        if n not in arr2:
+            tail.append(n)
+        else:
+            D[n] += 1
+    tail.sort()
+    for n in arr2:
+        res += [n] * D[n]
+    return res + tail
