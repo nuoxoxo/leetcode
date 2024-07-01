196fce36ff2bdd1f09532c1786045cb6678b4c11
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-01 16:37:35 +0200

    633/2 ways in cc

--- leet_BS_0633_sq_sums.cc
+++ leet_BS_0633_sq_sums.cc
@@ -1,24 +1,41 @@
-class Solution {
+func judgeSquareSum(c int) bool {
 
-#define ll long long
-
-public:
-    bool judgeSquareSum(int c) {
-        ll  b = sqrt(c);
-        ll  a = 0;
-        while (a <= b)
-        {
-            ll  p = a * a + b * b;
-            if (p == c)
-                return true;
-            if (p > c)
-                --b;
-            else
-                ++a;
-        }
-        return false;
+    solutions := []func(int)bool{
+        // Complement_set,
+        Binary_search,
     }
-};
+    return solutions[0](c)
+}
 
-/// test
-/// 15248456
+func Binary_search(c int) bool {
+
+    L, R := uint64(0), uint64(math.Sqrt(float64(c)))
+    for L <= R {
+        lhs := R*R + L*L
+        if lhs == uint64(c) {
+            return true
+        }
+        if lhs < uint64(c) {
+            L++
+        } else {
+            R--
+        }
+    }
+    return false
+}
+
+func Complement_set(c int) bool {
+    seen := make(map[int]bool)
+    n := 0
+    for n <= int(math.Sqrt(float64(c))) {
+        sq := n * n
+        seen[sq] = true
+        complt := c - sq
+        if seen[complt] {
+            return true
+        }
+        n++
+    }
+    return false
+}
+
