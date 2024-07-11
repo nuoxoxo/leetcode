c7e8d5a17d6392fa9340598c4ab2f95d59a8b789
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-11 22:56:49 +0200

    1190/cc - using deque + faster in-place soln

--- leet_1190_rev_substr.cc
+++ leet_1190_rev_substr.cc
@@ -2,6 +2,39 @@ class Solution {
 public:
     string reverseParentheses(string s)
     {
+        vector<string(*)(string)> solutions = {
+            // Initial_soln,
+            Faster_reverse_inplace,
+        };
+        return solutions[0]( s );
+    }
+
+    static string Faster_reverse_inplace(string s)
+    {
+        deque<int> Q;
+        string res;
+        for (char c : s)
+        {
+            if (c == '(')
+            {
+                Q.push_back( res.length() );
+            }
+            else if (c == ')')
+            {
+                int left = Q.back();
+                Q.pop_back();
+                reverse(begin(res) + left, end(res));
+            }
+            else
+            {
+                res += c;
+            }
+        }
+        return res;
+    }
+
+    static string Initial_soln(string s)
+    {
         deque<int> Q;
         string res;
         int N = s.length();
