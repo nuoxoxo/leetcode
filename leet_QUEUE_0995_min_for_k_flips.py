ac27e98eea4229f21302a6844b005bce0d3d5391
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-19 23:31:11 +0200

    995/short code hard pb. - added detailed notes

--- leet_QUEUE_0995_min_for_k_flips.py
+++ leet_QUEUE_0995_min_for_k_flips.py
@@ -1,21 +1,42 @@
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
+        # [1,1,0], k = 2
+        #   -> [1,0,1] -> we can flip these 2
+        #         ^ ^ 
+        #   -> [0,0,1] -> or we can flip these
+        #       ^ ^ 
+        #   -> impossible bc. a 0- or 1-bit is left either way
+        #
+        # [0,0,0,1,0,1,1,0], k = 3
+        #   -> [1,1,1,1,0,1,1,0] -> step1
+        #       ^ ^ ^ 
+        #   -> [1,1,1,1,1,0,0,0] -> step2
+        #               ^ ^ ^
+        #   then flip the final len-3 subarr
+        #
+        #       Strategy
+        # once we can flip we flip, store the window flipped, swicth state and gain 1
+        # once we meet the R-index of a window, dispose this window, swich the state
+        #       Tool
+        # a queue to store the window where flips happen.
+        # a window is identified by the R-index of the window.
         N = len(nums)
-        Q = deque()
-        curr = False # to flip or not
+        ENDS = deque()
+        flipping = 0 # imagine if [0] is a 0, we have no choice but flipping it
         res = 0
-        for i, n in enumerate(nums):
-            if Q and Q[0] == i:
-                curr = not curr
-                Q.popleft()
-            # once we're at a 0, flip next k bits
-            if n == curr:
+        for i, curr in enumerate(nums):
+            # once we meet an R-index of a marked subarr of len-k 
+            # we pop ie. flip the subarr[ prevIndex : prevIndex + k ]
+            if ENDS and ENDS[0] == i:
+                flipping ^= 1
+                ENDS.popleft()
+            if flipping == curr:
                 # flip next k bits
-                curr = not curr
-                Q.append(i + k)
+                flipping ^= 1
+                ENDS.append(i + k)
                 res += 1
-        if Q and Q[0] == N:
-            Q.popleft()
-        if Q:
+        if len(ENDS) > 1:
             return -1
-        return res
+        if not ENDS or ENDS[0] == N:
+            return res
+        return -1
