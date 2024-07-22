9abf99b7ac9d77b7454dc9a6e9c8240c23141950
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-22 10:07:13 +0200

    2418/simple oneliner

--- leet_2418_sort_people.py
+++ leet_2418_sort_people.py
@@ -1,7 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]: 
-        return [_[0] for _ in \
-            reversed(sorted(
-                list(zip(names, heights)),
-                key=lambda x: x[1])
-            )]
+        return [_[1] for _ in sorted([(h, n) for n, h in zip(names, heights)], reverse=True)]
