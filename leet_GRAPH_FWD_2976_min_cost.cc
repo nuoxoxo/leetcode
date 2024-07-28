59e131c2b1144ce44c4e5c6b05d1b8731f2d14b6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-28 21:22:50 +0200

    2976/cc: using dijkstra (done) + floyd warshall

--- leet_GRAPH_FWD_2976_min_cost.cc
+++ leet_GRAPH_FWD_2976_min_cost.cc
@@ -2,10 +2,89 @@ class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost)
     {
-        vector<long long(*)(string, string, vector<char>&, vector<char>&, vector<int>&)> Solutions = {
-            Floyd_Warshall_DP,
+        vector<long long(*)(string, string, 
+            vector<char>&, vector<char>&, vector<int>&)> Solutions = {
+            // Floyd_Warshall_DP,
+            Dijkstra,
         };
         return Solutions[0]( source, target, original, changed, cost );
+    }
+
+    static long long Dijkstra(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost)
+    {
+
+        #define ll long long
+
+        unordered_map<int, vector<pair<ll, int>>> W;
+        int len = original.size();
+        int i = -1;
+        while (++i < len)
+        {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            int c = cost[i];
+            W[u].push_back( {c, v} );
+        }
+
+        function<ll(int, int)> dijkstra = [&](int src, int des) {
+            priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> heap;
+            heap.push({0, src});
+            vector<ll> weights(26, LLONG_MAX);
+            set<int> SEEN;
+            while (!heap.empty())
+            {
+                auto p = heap.top();
+                heap.pop();
+                ll curr_cost = p.first;
+                int curr_node = p.second;
+                if (curr_node == des)
+                    return curr_cost;
+                if (SEEN.find(curr_node) == end(SEEN))
+                {
+                    SEEN.insert(curr_node);
+                    for (auto & _next : W[curr_node])
+                    {
+                        ll next_cost = _next.first;
+                        int next_node = _next.second;
+                        ll tt_cost = curr_cost + next_cost;
+                        if (tt_cost < weights[next_node])
+                        {
+                            weights[next_node] = tt_cost;
+                            heap.push( {tt_cost, next_node} );
+                        }
+                    }
+                }
+            }
+            return LLONG_MAX;
+        };
+
+
+        ll res = 0;
+        vector<vector<bool>> has_cache(26, vector<bool>(26, false));
+        vector<vector< ll >> cache(26, vector< ll >(26, 42));
+        i = -1;
+        int N = source.size();
+        while (++i < N)
+        {
+            int u = source[i] - 'a';
+            int v = target[i] - 'a';
+            if (u == v)
+                continue ;
+            if (has_cache[u][v])
+            {
+                res += cache[u][v];
+            }
+            else
+            {
+                ll c = dijkstra(u, v);
+                if (c == LLONG_MAX)
+                    return -1;
+                has_cache[u][v] = true;
+                cache[u][v] = c;
+                res += c;
+            }
+        }
+        return res;
     }
 
     static int idx(int n) { return n - 'a'; }
