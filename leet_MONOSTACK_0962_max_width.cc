985cc34e1680e6cfca16401ce36c9abeebd5f5c7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-10 17:31:40 +0200

    962/cc - using a monotonically decreasing stack

--- leet_MONOSTACK_0962_max_width.cc
+++ leet_MONOSTACK_0962_max_width.cc
@@ -2,26 +2,20 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums)
     {
-        deque<int>  dq;
-        int         n = (int) nums.size();
-        int         res = 0;
-        int         i;
-        
-        i = -1;
-        while (++i < n)
-        {
-            if (dq.empty() || nums[i] < nums[dq.front()])
-            {
-                dq.push_front(i);
-            }
-        }
-        i = n;
+        std::deque<int> Q;
+        int N = nums.size();
+        int i = -1;
+        while (++i < N)
+            if (Q.empty() || nums[Q.front()] > nums[i])
+                Q.push_front(i);
+        i = N;
+        int res = 0;
         while (--i > -1)
         {
-            while (!dq.empty() && nums[i] >= nums[dq.front()])
+            while ( ! Q.empty() && nums[Q.front()] <= nums[i])
             {
-                res = max(res, i - dq.front());
-                dq.pop_front();
+                res = max(res, i - Q.front());
+                Q.pop_front();
             }
         }
         return res;
