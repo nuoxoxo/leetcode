acd0e1b69625ebf4651622316920d81842b26bcc
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-11 16:08:34 +0200

    1942/smart way of using 2 heaps and treating arr/dep times as just stamps - add notes

--- leet_1942_keeping_2_heaps.py
+++ leet_1942_keeping_2_heaps.py
@@ -4,6 +4,11 @@ class Solution:
         # idea/ - two min heaps
         #   --> 1st heap keeps chairs now available
         #   --> 2nd heap keeps runnning events
+        #   (
+        #       arrival_time,
+        #       is_present, ---> will be int for heapQ sorting
+        #       idx, ---> i-th friend
+        #   )
 
         N = len(times)
         chairs = []
@@ -11,45 +16,27 @@ class Solution:
             heapq.heappush(chairs, i)
 
         events = []
-        staying, hasleft = 1, 0 # 2 states
-        for i, (arrive, _) in enumerate(times): # <--- we dont care abt departure time
-            heapq.heappush(events, (arrive, staying, i))
+        willstay, hasleft = 1, 0 # 2 states
+        for idx, (arrive, _) in enumerate(times): # <--- we dont care abt departure time
+            heapq.heappush(events, (arrive, willstay, idx))
 
-        # main soln
+        print(heapq.nsmallest(len(events), events)) # <--- here's how to print heapQ
+        # [[1,4],[2,3],[4,6]]
+        # ---heapified events---> [(1, 1, 0), (2, 1, 1), (4, 1, 2)]
+
+        # [[3,10],[1,5],[2,6]]
+        # ---heapified events---> [(1, 1, 1), (2, 1, 2), (3, 1, 0)]
+
+        # soln
         while True:
-            arrive, state, idx = heapq.heappop(events)
+            arrive, state, friend = heapq.heappop(events)
             if state == hasleft:
-                heapq.heappush(chairs, idx)
+                heapq.heappush(chairs, friend)
             else:
-                chair = heapq.heappop(chairs)
-                if idx == targetFriend:
-                    return chair
-                leavingtime = times[idx][1] # <--- track leaving by pushing timestamp of leaving
-                heapq.heappush(events, ( leavingtime, hasleft, chair))
+                chair_to_free = heapq.heappop(chairs)
+                if friend == targetFriend:
+                    return chair_to_free
+                leavingtime = times[friend][1] # <--- track leaving by pushing timestamp of leaving
+                heapq.heappush(events, ( leavingtime, hasleft, chair_to_free))
         assert False
 
-        # dont know what goes wrong
-        """
-        t = [ [i + 1, _[0], _[1]] for i, _ in enumerate(times) ]
-        t.sort(key=lambda x: x[1])
-        while True:
-            # print("bef/", t)
-            todos = []
-            for i, line in enumerate(t):
-                _, arr, cnt = line
-                if arr < 2 and cnt == 0:
-                    todos.append(i)
-            for i in range(len(todos) - 1, -1, -1):
-                del t[todos[i]]
-            # print("aft/", t)
-            for i, friend in enumerate(t):
-                idx, arr, _ = friend
-                if idx == targetFriend + 1 and arr == 1:
-                    # print("res/", t, '\n')
-                    return i
-                if arr < 2:
-                    t[i][2] -= 1
-                else:
-                    t[i][1] -= 1
-        assert False
-        """
