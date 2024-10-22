5240b591ff3b4ae9a969ec8251ad9570eb8f9aa0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-22 10:10:04 +0200

    2583/bfs - count tt levels & get all vals, sort em and there we have kth val

--- leet_BFS_2583_kth_largest.go
+++ leet_BFS_2583_kth_largest.go
@@ -7,32 +7,39 @@
  * }
  */
 func kthLargestLevelSum(root *TreeNode, k int) int64 {
+    solutions := []func(*TreeNode, int)int64{
+        BFS,
+    }
+    return solutions[0]( root, k )
+}
+
+func BFS(root *TreeNode, k int) int64 {
 	D := []*TreeNode{root}
 	res := []int64{}
 	for len(D) > 0 {
 		var level int64 = 0
-		len := len(D)
-		for len > 0 {
-            len--
+	    N := len(D)
+		for N > 0 {
 			node := D[0]
 			D = D[1:]
-			level += int64(node.Val)
-			if node.Left != nil {
-				D = append(D, node.Left)
-			}
-			if node.Right != nil {
-				D = append(D, node.Right)
-			}
+			if node.Left != nil { D = append(D, node.Left) }
+			if node.Right != nil { D = append(D, node.Right) }
+            level += int64(node.Val)
+            N--
 		}
 		res = append(res, level)
 	}
 
-	n := len(res)
-	if n < k {
+	N := len(res)
+	if N < k {
 		return -1
 	}
 	sort.Slice(res, func(i, j int) bool {
 		return res[i] > res[j]
 	})
 	return res[k - 1]
+
+    // sort.Ints(res) // bc. it's a int64 slice so we can't use Ints
+    // return res[N - k]
+
 }
