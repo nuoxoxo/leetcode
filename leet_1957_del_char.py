e761a1be6985349ec9197c4fe32fc53e1ad1f518
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-11-01 19:38:37 +0100

    1957/better way

--- leet_1957_del_char.py
+++ leet_1957_del_char.py
@@ -1,24 +1,45 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        arr = []
-        i = 0
-        N = len(s)
-        prev = None
-        count = 1
-        while i < N:
-            if s[i] == prev:
-                count += 1
-            else:
-                if prev:
-                    arr.append(prev)
-                    arr.append(count)
-                prev = s[i]
-                count = 1
-            i += 1
-        arr.append(prev)
-        arr.append(count)
-        res = ''
-        for i in range(0, len(arr), 2):
-            n = 2 if arr[i + 1] > 2 else arr[i + 1]
-            res += arr[i] * n
-        return res
+        return [
+            # Initial_soln,
+            better,
+        ][0](s)
+
+def better(s: str) -> str:
+    prev = None
+    count = 1
+    res = ''
+    for c in s:
+        if c != prev:
+            count = 1
+            prev = c
+            res += c
+        elif count < 2:
+            count += 1
+            res += c
+    return res
+
+
+def Initial_soln(s: str) -> str:
+    arr = []
+    i = 0
+    N = len(s)
+    prev = None
+    count = 1
+    while i < N:
+        if s[i] == prev:
+            count += 1
+        else:
+            if prev:
+                arr.append(prev)
+                arr.append(count)
+            prev = s[i]
+            count = 1
+        i += 1
+    arr.append(prev)
+    arr.append(count)
+    res = ''
+    for i in range(0, len(arr), 2):
+        n = 2 if arr[i + 1] > 2 else arr[i + 1]
+        res += arr[i] * n
+    return res
