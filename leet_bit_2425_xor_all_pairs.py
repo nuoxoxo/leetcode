5e2c46b698e95585b43e55c7c4de397d392c0c52
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-01-16 21:29:13 +0100

    2425/ simple math - fixed

--- leet_bit_2425_xor_all_pairs.py
+++ leet_bit_2425_xor_all_pairs.py
@@ -9,14 +9,14 @@ class Solution:
         #   [ a^x^a^y^a^z^b^x^b^y^b^z] /nums3 xored
         #       ^   ^   ^   ^   ^   ^ --> x^x^y^y^z^z = 0 bc. len1 is even
         #     ^   ^   ^ --> a^a^a AND also b^b^b --> a ^ b bc. len2 is odd
-        x1, x2 = 0, 0
         n1, n2 = len(nums1), len(nums2)
-        for n in nums1: x1 ^= n
-        for n in nums2: x2 ^= n
         if n1 % 2 == 0 and n2 % 2 == 0:
             return 0
+        x1, x2 = 0, 0
+        for n in nums1: x1 ^= n
         if n1 % 2 == 0:
             return x1
+        for n in nums2: x2 ^= n
         if n2 % 2 == 0:
             return x2
         return x1 ^ x2
