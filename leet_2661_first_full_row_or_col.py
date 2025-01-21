11d39e6e6a676370439b6386d99c6f0ef5fa41ac
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-01-21 08:56:05 +0100

    2661/fixed to simpler counting for python

--- leet_2661_first_full_row_or_col.py
+++ leet_2661_first_full_row_or_col.py
@@ -1,22 +1,18 @@
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         R,C = len(mat), len(mat[0])
-        rows,cols,rowcount,colcount = {},{},collections.defaultdict(int),collections.defaultdict(int)
+        rc,rowcount,colcount = {},[0] * R, [0] * C
         for r in range(R):
             for c in range(C):
                 n = mat[r][c]
-                rows[n] = r
-                cols[n] = c
-        # print(rows,cols)
+                rc[n] = (r,c)
         for i,n in enumerate(arr):
-            r,c = rows[n],cols[n]
+            r,c = rc[n]
             rowcount[r] += 1
             if rowcount[r] == C: # bugfix/ we check if any cell on row 'r' reaches C
-                # print('r/',n,r,rowcount[r],R)
                 return i
             colcount[c] += 1
             if colcount[c] == R:
-                # print('c/',n,c,colcount[r],C)
                 return i
         assert False
 
