e6844b3ba3c6632a4516d85a436db74620619cf2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-03-03 09:14:23 +0100

    2570/ed. shorter

--- leet_2PTR_2570_merge_arrays.cc
+++ leet_2PTR_2570_merge_arrays.cc
@@ -3,43 +3,23 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2)
     {
         vector<vector<int>> res;
-        int i, j;
-        i = j = 0;
+        int i = 0, j = 0;
         int len1 = nums1.size();
         int len2 = nums2.size();
-        while (i < len1 && j < len2)
-        {
-            int a = nums1[i][0], b = nums1[i][1];
-            int c = nums2[j][0], d = nums2[j][1];
-            if (a == c)
-            {
+        while (i < len1 && j < len2) {
+            int a,b,c,d;
+            a = nums1[i][0], b = nums1[i][1], c = nums2[j][0], d = nums2[j][1];
+            if (a == c && ++i && ++j)
                 res.push_back({ a, b + d });
-                ++i;
-                ++j;
-            }
-            else if (a < c)
-            {
+            else if (a < c && ++i) 
                 res.push_back({ a, b });
-                ++i;
-            }
-            else if (a > c)
-            {
+            else if (a > c && ++j)
                 res.push_back({ c, d });
-                ++j;
-            }
         }
         while (i < len1)
-        {
-            int a = nums1[i][0], b = nums1[i][1];
-            res.push_back({nums1[i][0], nums1[i][1]});
-            ++i;
-        }
+            res.push_back(nums1[i++]);
         while (j < len2)
-        {
-            int a = nums2[j][0], b = nums2[j][1];
-            res.push_back({nums2[j][0], nums2[j][1]});
-            ++j;
-        }
+            res.push_back(nums2[j++]);
         return res;
     }
 };
