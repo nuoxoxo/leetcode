de940eab65302b8c67e7cdd6eeb67044dbd6533a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2025-04-24 20:02:11 +0200

    2799/added straightforward soln

--- leet_SLWD_2799_count_subarr.py
+++ leet_SLWD_2799_count_subarr.py
@@ -1,19 +1,37 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        N = len(nums)
-        L, R = 0,0
-        S = set(nums)
-        UN = len(S)
-        D = collections.defaultdict(int)
-        res = 0
-        while R < N:
-            D[nums[R]] += 1
-            while len(D) == UN:
-                D[nums[L]] -= 1
-                res += N - R
-                if D[nums[L]] == 0:
-                    D.pop(nums[L])
-                L += 1
-            R += 1
-        return res
+        return [
+            direct_solution,
+            # using_dict,
+        ][0](nums)
+
+def direct_solution(nums: List[int]) -> int:
+    UN = len(set(nums))
+    N = len(nums)
+    res = 0
+    for L in range(N):
+        S = set()
+        for R in range(L, N):
+            S.add(nums[R])
+            if len(S) == UN:
+                res += 1
+    return res
+
+def using_dict(nums: List[int]) -> int:
+    N = len(nums)
+    L, R = 0,0
+    UN = len(set(nums))
+    D = collections.defaultdict(int)
+    res = 0
+    while R < N:
+        # print('L/R -',L,R)
+        D[nums[R]] += 1
+        while len(D) == UN:
+            D[nums[L]] -= 1
+            if D[nums[L]] == 0:
+                D.pop(nums[L])
+            L += 1
+            res += N - R
+        R += 1
+    return res
 
